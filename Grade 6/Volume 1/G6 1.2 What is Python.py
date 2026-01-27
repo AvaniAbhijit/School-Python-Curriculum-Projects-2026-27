40643548bd3ec97d4575011d2cbe675a2073322e
Avani <avani.abhijit@orchids.edu.in>
2026-01-27 15:22:42 +0530

    Update G6 1.2 What is Python.py

--- Grade 6/Volume 1/G6 1.2 What is Python.py
+++ Grade 6/Volume 1/G6 1.2 What is Python.py
@@ -1,5 +1,17 @@
+# What is Python?
+# Python is a programming language used for mathematical operations, 
+# game development, website development, software development, etc.
+
+# What print()?
+# The print statement is used to show information on the screen.
+# Whatever we write inside print() will appear as output when the program runs.
+
+# What is a comment in Python?
+# is used to comment out the code in Python. Comments are information about the code for the reader.)
+
+
 # Task 1: Run the code and check the output.
-# Task 2: Change the number for addition(line no.14) and multiplication (line no. 16).
+# Task 2: Change the number for addition(line no.26) and multiplication (line no. 28).
 # Task 3: Write the print statement to subtract two numbers
 
 
