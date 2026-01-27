b87605366b4955ba4a2174cbb0ef83aae3c4293d
Avani <avani.abhijit@orchids.edu.in>
2026-01-27 11:14:32 +0530

    Add files via upload

---
+++ Grade 8/Volume 1/G8 1.2  What is Python.py
@@ -0,0 +1,16 @@
+# Task 1: Run the code and check the output.
+# Task 2: Change the number for addition(line no.14) and multiplication (line no. 16).
+# Task 3: Write the print statement to subtract two numbers
+
+
+# Python helps us print a one-line statement.
+print("Hello, students!")
+
+# Python helps us print multiple lines.
+print("Welcome to our Python class.")
+print("Let's learn to code!")
+
+# Python solves math problems.
+print(5 + 6)  # Addition example
+
+print(7*3)
---
+++ Grade 8/Volume 1/G8 1.3 Variables & Data Types.py
@@ -0,0 +1,50 @@
+# Task 1: Run the code and check the output.
+# Task 2: Change the values of the variables.
+# Task 3: Create one new variable of your choice and print it.
+
+
+# Python uses variables to store information.
+name = "Aarav"
+age = 13
+
+print(name)
+print(age)
+
+
+# Python supports different data types.
+
+# 1. String (text data)
+school = "Green Valley School"
+print(school)
+
+
+# 2. Integer (whole numbers)
+marks = 85
+print(marks)
+
+
+# 3. Float (decimal numbers)
+height = 5.6
+print(height)
+
+
+# 4. Boolean (True or False)
+is_student = True
+print(is_student)
+
+
+# Python can also tell us the data type of a variable.
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+
+# Python allows math operations using variables.
+a = 10
+b = 5
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
