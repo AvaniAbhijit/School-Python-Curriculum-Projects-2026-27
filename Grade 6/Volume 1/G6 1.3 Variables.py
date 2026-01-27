518b47b56d6f7887ab89ea5259804e33ea73403d
Avani <avani.abhijit@orchids.edu.in>
2026-01-27 15:39:51 +0530

    Add files via upload

---
+++ Grade 6/Volume 1/G6 1.3 Variables.py
@@ -0,0 +1,32 @@
+# What is variables?
+# Variables are used to store information in a program.
+# The value of a variable can be changed while the program is running.
+
+# Rules of variables :
+# 1. Variable name must start with a letter or underscore (_).
+#       It cannot start with a number.
+# 2. Variable names should not contain spaces.
+#       Use underscore (_) instead of space.
+# 3. Variable names are case-sensitive.
+#       Age and age are treated as different variables.
+# 4. Variable names should be meaningful.
+#       This helps us understand what data is stored in the variable.
+# 5. Numbers and special symbols are not allowed in variable names.
+#       Only letters, numbers (not at start), and underscore (_) are allowed.
+
+
+# Task 1: Run the code and check the output.
+# Task 2: Change the values of the variables.
+# Task 3: Create one new variable by following the variable rules.
+# Task 4:Uncomment line 31 & 32 and run the code, fix the error.
+
+
+
+student_name = "Riya"
+student_age = 11
+
+print(student_name)
+print(student_age)
+
+#2student_marks=84
+#print(2student_marks)
---
+++ Grade 6/Volume 1/G6 1.4  Data Types.py
@@ -0,0 +1,51 @@
+# What are Data Types?
+# Data types tell us what kind of data a variable is storing.
+# Python uses different data types to store different kinds of values.
+
+# Types of Data Types:
+
+# 1. String
+# Used to store text data.
+# Text is always written inside quotes (" ").
+
+# 2. Integer
+# Used to store whole numbers.
+# It does not contain decimal values.
+
+# 3. Float
+# Used to store decimal numbers.
+# It contains a decimal point.
+
+# 4. Boolean
+# Used to store True or False values.
+# The first letter must be capital (True / False).
+
+
+# Task 1: Run the code and check the output.
+# Task 2: Change the values of the variables.
+# Task 3: Create one new variable for each data type.
+# Task 4: Check the data type of all variables using type().
+
+
+# String data type
+student_name = "Riya"
+print(student_name)
+
+# Integer data type
+student_age = 11
+print(student_age)
+
+# Float data type
+percentage = 92.5
+print(percentage)
+
+# Boolean data type
+is_student = True
+print(is_student)
+
+
+# Checking the data type of variables
+print(type(student_name))
+print(type(student_age))
+print(type(percentage))
+print(type(is_student))
