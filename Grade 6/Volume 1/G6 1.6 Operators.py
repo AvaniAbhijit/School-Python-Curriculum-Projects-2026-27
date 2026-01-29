6a7daff1a9ead4df9c3d8eda602e766435674697
Avani <avani.abhijit@orchids.edu.in>
2026-01-29 12:33:37 +0530

    Add files via upload

---
+++ Grade 6/Volume 1/G6 1.6 Operators.py
@@ -0,0 +1,30 @@
+# Operators are symbols used to perform operations on values.
+# They help us compare values and make decisions in a program.
+
+# Types of Operators covered here:
+# 1. Relational Operators (< (Less than) , > (Greater than))
+# 2. Logical Operators (and , or)
+
+# Task 1: Change the value of num and check the output
+# Task 2: Try different numbers and observe True / False
+# Task 3: Create two variables and use < , > , and , or
+
+
+# Relational Operators
+print(5 < 10)   # True
+print(8 > 15)   # False
+
+# Logical Operators
+# and operator - Returns True if BOTH conditions are True
+age = 12
+print(age > 10 and age < 15)   # True
+
+# or operator - Returns True if ANY ONE condition is True
+marks = 35
+print(marks > 90 or marks > 30)   # True
+
+
+
+
+
+
