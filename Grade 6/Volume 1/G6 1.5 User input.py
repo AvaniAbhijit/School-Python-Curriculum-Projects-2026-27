327d5ca760274e68105ec1eea4806600256bdc36
Avani <avani.abhijit@orchids.edu.in>
2026-01-29 12:08:41 +0530

    Add files via upload

---
+++ Grade 6/Volume 1/G6 1.5 User input.py
@@ -0,0 +1,32 @@
+# What is User Input?
+# User input means taking information from the user while the program is running.
+# Python uses the input() function to take input from the user.
+
+# Important Point:
+# input() always takes the input as text (string).
+
+# How to convert input:
+# int()   → converts input to integer
+# float() → converts input to decimal number
+
+
+# Task 1: Run the code and enter values.
+# Task 2: Uncomment the 30 to 32 and run the code, Fix the error if any.
+#
+
+
+# Taking string input
+student_name = input("Enter student name: ")
+print(student_name)
+
+# Taking integer input
+student_age = int(input("Enter student age: "))
+print(student_age)
+
+# Taking float input
+percentage = float(input("Enter percentage: "))
+print(percentage)
+
+#number = input("Enter a number: ")
+#print(number)
+#print(number + 5)
