d6c656453865b83687b98bf866c848c7aa055616
Avani <avani.abhijit@orchids.edu.in>
2026-02-09 21:09:33 +0530

    Update G6 1.3 Variables.py

--- Grade 6/Volume 1/G6 1.3 Variables.py
+++ Grade 6/Volume 1/G6 1.3 Variables.py
@@ -9,10 +9,10 @@
 #       Use underscore (_) instead of space.
 # 3. Variable names are case-sensitive.
 #       Age and age are treated as different variables.
-# 4. Variable names should be meaningful.
-#       This helps us understand what data is stored in the variable.
-# 5. Numbers and special symbols are not allowed in variable names.
+# 4. Numbers and special symbols are not allowed in variable names.
 #       Only letters, numbers (not at start), and underscore (_) are allowed.
+
+# Variable names should be meaningful. This helps us understand what data is stored in the variable.
 
 
 # Task 1: Run the code and check the output.
