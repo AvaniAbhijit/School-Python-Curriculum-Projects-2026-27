dd1d646df62d9064686eea47a1f71aa6929f2b3c
Avani <avani.abhijit@orchids.edu.in>
2026-02-10 12:57:23 +0530

    Update and rename G6 1.6 Operators.py to G6 1.6 .1 Relational Operators.py

---
+++ Grade 6/Volume 1/G6 1.6 .1 Relational Operators.py
@@ -0,0 +1,14 @@
+# Operators are symbols used to perform operations on values.
+# They help us compare values and make decisions in a program.
+
+# Types of Operators covered here:
+# 1. Relational Operators (< (Less than) , > (Greater than))
+
+# Task 1: Run the code and check the output
+# Task 2: Write the code to check if 121 is greater than 200 or not on line 14 onwards. 
+
+# Relational Operators example.
+print(5 < 10)   # True
+print(8 > 15)   # False
+
+
