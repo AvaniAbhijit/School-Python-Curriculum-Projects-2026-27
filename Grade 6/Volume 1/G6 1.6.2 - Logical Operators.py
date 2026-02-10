a7277b2d97417a2c65d1c053d86cfff42f944a57
Avani <avani.abhijit@orchids.edu.in>
2026-02-10 13:01:19 +0530

    Add files via upload

---
+++ Grade 6/Volume 1/G6 1.6.2 - Logical Operators.py
@@ -0,0 +1,18 @@
+# Operators are symbols used to perform operations on values.
+# They help us compare values and make decisions in a program.
+
+# Types of Operators covered here:
+# 2. Logical Operators (and , or)
+
+# Task 1: Run the code and check the output.
+# Task 2: Combine and and or Operators to create a condition on line 19 onwords.
+
+# Logical Operators
+# and operator - Returns True if BOTH conditions are True
+age = 12
+print(age > 10 and age < 15)   # True
+
+# or operator - Returns True if ANY ONE condition is True
+marks = 35
+print(marks > 90 or marks > 30)   # True
+
