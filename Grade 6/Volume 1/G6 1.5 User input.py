318b9ee0cd898f6677fa04d9624ff7be50a9d8d5
Avani <avani.abhijit@orchids.edu.in>
2026-02-10 12:53:30 +0530

    Update G6 1.5 User input.py

--- Grade 6/Volume 1/G6 1.5 User input.py
+++ Grade 6/Volume 1/G6 1.5 User input.py
@@ -9,23 +9,24 @@
 # int()   → converts input to an integer.
 # float() → converts input to a decimal number.
 
-
-# Task 1: Run the code and enter values.
-# Task 2: Uncomment the 30 to 32 and run the code. Fix the error if any.
-
-
+# Task 1: Run the code and print the student_name on line 18.
+# Task 2: Write the code to check the type of student_name and the student's age variable on lines 19 and 24.
+# Task 3: Uncomment lines 31-33 and fix the error.
 
 # Taking string input
 student_name = input("Enter student name: ")
-print(student_name)
+
+
 
 # Taking integer input
 student_age = int(input("Enter student age: "))
 print(student_age)
 
+
 # Taking float input
 percentage = float(input("Enter percentage: "))
 print(percentage)
+
 
 #number = input("Enter a number: ")
 #print(number)
