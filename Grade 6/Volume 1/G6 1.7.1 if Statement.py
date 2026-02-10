72e8c4e000f425e8103abd01f2324b8a4bdcbf21
Avani <avani.abhijit@orchids.edu.in>
2026-02-10 21:29:54 +0530

    Add files via upload

---
+++ Grade 6/Volume 1/G6 1.7.1 if Statement.py
@@ -0,0 +1,20 @@
+# Conditional statements are used to make decisions in a program.
+# They check a condition and run code based on True or False.
+
+# Types of Conditional Statements:
+# 1. if statement - if runs when the condition is True.
+
+# Task 1: Uncomment line 14 & 15 and complete the code.
+# Task 2: Uncomment line 17 & 18 and complete the code.
+
+age = 12
+if age > 10:
+    print("You are older than 10")
+
+number = 7
+#if ____:
+#    print("Number is greater than 5")
+
+temperature = 30
+#if ____:
+#    print("It is a hot day,")
---
+++ Grade 6/Volume 1/G6 1.7.2. If-else statement.py
@@ -0,0 +1,22 @@
+# Conditional statements are used to make decisions in a program.
+# They check a condition and run code based on True or False.
+
+# Types of Conditional Statements:
+# 2. if-else statement - if runs when the condition is True
+#                        else runs when the condition is False.
+
+# Task 1: Uncomment line 19 to 22 and complete the code.
+# Task 2:Take a number from the user,If the number is even, print "Even Number"
+#        Else, print "Odd Number"
+
+marks = 35
+if marks > 40:
+    print("You passed")
+else:
+    print("You failed")
+
+age = 15
+#if age >= __________:
+#    print("You can vote")
+#else:
+#    print("You cannot vote")
