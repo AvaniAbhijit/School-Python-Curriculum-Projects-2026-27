0bce52f2822c4399c39e8de1eee334970d4bfd08
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 09:35:15 +0530

    Update G6 2.5 Tuple.py

--- Grade 6/Volume 1/G6 2.5 Tuple.py
+++ Grade 6/Volume 1/G6 2.5 Tuple.py
@@ -2,25 +2,19 @@
 # A tuple is used to store multiple values in one variable.
 # A tuple is written using round brackets( ).
 
-# Task 1: Create a tuple and print it.
-# Task 2: Print the length of the tuple.
-# Task 3: Print the first item of the tuple using index.
-# Task 4: Print all items in the tuple using a for loop.
+# Task 1: Print the length of the tuple on a line on line 14.
+# Task 2: Print the first item of the tuple using index on line 17.
+# Task 3: Uncomment line 20 and print the tuple and solve the error.
 
 # Creating a Tuple
 num = (11,55,66,33)
 print(num)
 
-# Length of a Tuple - len() gives the number of items in the tuple
-print(len(num))
+# print length of a Tuple
+
 
 # Accessing Items using Index - starts from 0
-print(num[0])   # First item
-print(num[2])   # Third item
 
-# Looping through a Tuple
-for n in num:
-    print(num)
 
 # Tuples cannot be changed. We cannot add or remove items from a tuple.
 # num.append(99)
