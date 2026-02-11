80f26967147a44ea904ddcc25c1d821326c2ff78
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 09:45:18 +0530

    Update G6 2.6 Dictionaries.py

--- Grade 6/Volume 1/G6 2.6 Dictionaries.py
+++ Grade 6/Volume 1/G6 2.6 Dictionaries.py
@@ -1,11 +1,11 @@
 # What is a Dictionary?
-# A dictionary stores data in key : value format.
+# A dictionary stores data in key: value format.
 # It is written using { }.
 # We can add, change, or remove items.
 
-# Task 1: Create a dictionary of a car.
-# Task 2: Print the brand name using key.
-# Task 3: Print all items using a for loop.
+# Task 1: Print the dictionary on line 19.
+# Task 2: Add an item to the dictionary using mobile["storage"] = "128GB" and print on line 22.
+# Task 3: Remove item from dictionary using pop(color) and print updated dictionary on line 26.
 
 # Create a dictionary of a mobile phone
 mobile = {
@@ -16,27 +16,10 @@ mobile = {
 }
 
 # Print the dictionary
-print("Mobile Details:", mobile)
 
-# Print total number of items
-print("Total items:", len(mobile))
-
-# Print the brand name
-print("Brand:", mobile["brand"])
-
-# Print all details using loop
-print("All Details:")
-for key in mobile:
-    print(key, ":", mobile[key])
-
-# Change the price
-mobile["price"] = 14000
-print("Updated Price:", mobile)
 
 # Add a new item
-mobile["storage"] = "128GB"
-print("After Adding Storage:", mobile)
 
-# Remove an item
-mobile.pop("color")
-print("After Removing Color:", mobile)
+
+
+# Remove an item using pop().
