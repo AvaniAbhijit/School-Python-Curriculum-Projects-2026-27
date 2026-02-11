953e66a39d2f5c7267e531e47920e6c8b3fee624
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 09:30:21 +0530

    Update G6 2.4 List.py

--- Grade 6/Volume 1/G6 2.4 List.py
+++ Grade 6/Volume 1/G6 2.4 List.py
@@ -2,30 +2,22 @@
 # A list is used to store multiple values in one variable.
 # A list is written using square brackets [ ].
 
-# Task 1: Create a list and print it.
-# Task 2: Print the length of the list.
-# Task 3: Print the first item of the list using index.
-# Task 4: Print all items in the list using a for loop.
+# Task 2: Find and print how many items are in the list using len function on line 15.
+# Task 3: Print the second item of the list using index on line 19.
+# Task 4: Print all items in the list using a for loop on line 23.
 
 # Creating a List
 subjects = ["Coding", "Horticulture", "Robotics"]
 print(subjects)
 
-# Length of a List - len() gives the number of items in the list
-print(len(subjects))
+# Length of a List
+# print the length of the list
 
-# Accessing Items using Index - Index starts from 0
-print(subjects[0])   # First item
-print(subjects[2])   # Third item
+
+# Accessing Items using Index
+# print the first item of the list
+
 
 # Looping through a List
-for sub in subjects:
-    print(subjects)
+# use a for loop to print each item
 
-# Adding an Item to the List - append() adds an item at the end of the list
-subjects.append("DIY")
-print(subjects)
-
-# Removing an Item from the List - remove() removes a specific item from the list
-subjects.remove("Horticulture")
-print(subjects)
