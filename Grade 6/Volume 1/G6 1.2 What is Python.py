a4e6a20832df2ef7977f682f63a65bf060112645
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 21:15:13 +0530

    Update G6 1.2 What is Python.py

--- Grade 6/Volume 1/G6 1.2 What is Python.py
+++ Grade 6/Volume 1/G6 1.2 What is Python.py
@@ -10,8 +10,7 @@
 # For comment in python # is used.Comments provide information about the code for the reader.
 
 # Task 1: Uncomment the line 24 and 26 and run the code.
-# Task 2: Enter actual number in place of number 1 and number 2 on line no 24 and 26.
-
+# Task 2: Change the number of your choice on lines 23 and 25.
 
 # Python helps us print a one-line statement.
 print("Hello, students!")
@@ -21,6 +20,6 @@ print("Welcome to our Python class.")
 print("Let's learn to code!")
 
 # Python solves math problems.
-# print(number1 + number2)  # Addition example
+# print(12 + 2)  # Addition example
 
-# print(number1 * number2)    # Multiplication example
+# print(2 * 8)    # Multiplication example
