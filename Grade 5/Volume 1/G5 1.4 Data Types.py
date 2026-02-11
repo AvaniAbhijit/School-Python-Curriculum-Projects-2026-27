6fe311bf54acf5ece7525656f328ac01a59972ac
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 22:10:58 +0530

    Update G5 1.4 Data Types.py

--- Grade 5/Volume 1/G5 1.4 Data Types.py
+++ Grade 5/Volume 1/G5 1.4 Data Types.py
@@ -3,37 +3,26 @@
 # Python uses different data types to store different kinds of values.
 
 # Types of Data Types:
-
-# 1. String
-# Used to store text data.
+# 1. String - Used to store text data. 
 # Text is always written inside quotes (" ").
 
-# 2. Integer
-# Used to store whole numbers.
+# 2. Integer - Used to store whole numbers.
 # It does not contain decimal values.
 
-# 3. Float
-# Used to store decimal numbers.
-# It contains a decimal point.
-
-# 4. Boolean
-# Used to store True or False values.
+# 3. Boolean - Used to store True or False values.
 # The first letter must be capital (True / False).
 
-#Task 1 : Print all variables from line 30 onwards.
-#Task 2 : Check the datatype of the variable marks,height and is_present.
+#Task 1: Print all variables from line 30 onwards.
+#Task 2: Check the datatype of the variable marks and is_present.
 
-favorite_food = "Pizza"   # String
-marks = 95                # Integer
-height = 4.5              # Float
-is_present = False        # Boolean
+favorite_food = "Pizza"     # String
+marks = 95                  # Integer
+is_present = False          # Boolean
 
-                        # print variable favorite_food
-                        # print variable marks
-                        # print variable height
-                        # print variable is_present
+                           # print variable favorite_food
+                           # print variable marks
+                           # print variable is_present
 
 print(type(favorite_food))  # check datatype for favorite_food
-                            # check datatype for marks
-                            # check datatype for height
+                            # check the datatype for marks
                             # check datatype for is_present
