cdcb2f424e8c9db0e1a438cff75b2d38f8028632
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 17:05:43 +0530

    Update G8 1.3 Variables & Data Types.py

--- Grade 8/Volume 1/G8 1.3 Variables & Data Types.py
+++ Grade 8/Volume 1/G8 1.3 Variables & Data Types.py
@@ -1,32 +1,37 @@
 # Variables are used to store information in a program. 
 # The value of a variable can be changed while the program is running.
+# Rules of variables :
+# 1. Variable name must start with a letter or underscore (_).
+#       It cannot start with a number.
+# 2. Variable names should not contain spaces.
+#       Use underscore (_) instead of space.
+# 3. Variable names are case-sensitive.
+#       Age and age are treated as different variables.
+# 4. Numbers and special symbols are not allowed in variable names.
+#       Only letters, numbers (not at start), and underscore (_) are allowed.
 # Data types tell Python what kind of value is stored in a variable, such as a number,
 # text, or True/False.
 
 
-# Task 1: Print the variable value and its type using type(variable_name) on lines 20 and 21.
-# Task 2: Change the values of the variables.
-# Task 3: Create one new variable of your choice and print it.
-
+# Task 1: Print the variable value and its type using type(variable_name).
 
 # 1. String (text data)
 school = "Green Valley School" # school is a variable here, and "Green Valley School" is a value.
-                               # print variable value.
-                               # print variable data type.
+print(school)                  # print variable value.
+print(type(school))            # print variable data type.
 
 # 2. Integer (whole numbers)
                               # Create a variable that has an integer value.
                               # print variable value.
                               # print variable data type.
 
-
-
 # 3. Float (decimal numbers)
-height = 5.6
-print(height)
-
+                              # Create a variable that has an integer value.
+                              # print variable value.
+                              # print variable data type.
 
 # 4. Boolean (True or False)
-is_student = True
-print(is_student)
+                              # Create a variable that has an integer value.
+                              # print variable value.
+                              # print variable data type.
 
