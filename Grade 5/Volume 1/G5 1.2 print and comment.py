805312de7d54726b63a7cc1fc11b076e4c368e92
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 21:21:35 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 1.2 print and comment.py
@@ -0,0 +1,25 @@
+# What is Python?
+# Python is a programming language used for mathematical operations,
+# game development, website development, software development, etc.
+
+# What is print()?
+# The print statement is used to show information on the screen.
+# Whatever we write inside print() will appear as output when the program runs.
+
+# What is a comment in Python?
+# For comment in python # is used.Comments provide information about the code for the reader.
+
+# Task 1: Uncomment the line 24 and 26 and run the code.
+# Task 2: Change the number of your choice on lines 23 and 25.
+
+# Python helps us print a one-line statement.
+print("Hello, students!")
+
+# Python helps us print multiple lines.
+print("Welcome to our Python class.")
+print("Let's learn to code!")
+
+# Python solves math problems.
+# print(12 + 2)  # Addition example
+
+# print(2 * 8)    # Multiplication example
