c42349ea82ea2623b2d4b95a296534f1be3118fa
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 22:14:13 +0530

    Update G5 1.3 Variables.py

--- Grade 5/Volume 1/G5 1.3 Variables.py
+++ Grade 5/Volume 1/G5 1.3 Variables.py
@@ -1,26 +1,22 @@
 # What are variables?
-# A variable is like a box.We use it to store information.
-# We can change what is inside the box anytime.
+# A variable is like a box. We use it to store information. We can change what is inside the box anytime.
 
 # Rules of variables :
 # 1. Start with a letter or _
-#       Correct → age, _score
-#       Wrong → 2age
+#       Correct → age, _score,  Wrong → 2age
 
 # 2. No spaces
-#       Correct → student_marks
-#       Wrong → student marks
+#       Correct → student_marks, Wrong → student marks
 
 # 3. Small and capital letters are different
-#       Age and age are NOT same.
+#       Age and age are NOT the same.
 
 # 4. No special symbols
-#        Correct → score1
-#        Wrong → score@
+#        Correct → score1, Wrong → score@
 
-# Task 1: Enter the values for the variable on line 25,28 and 31.
+# Task 1: Enter the values for the variable on line 21,24 and 27.
 # Task 2: fix the errors.
-# Task 3: Create variable on line 35 and print it on line 36.
+# Task 3: Create a variable on line 31 and print it on line 32.
 
 my name = "_____"
 print(my_name)
