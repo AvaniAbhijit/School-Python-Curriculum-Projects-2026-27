66e3c148e0c2ff7bacfecc8b722e187ab2f4bdc3
Avani <avani.abhijit@orchids.edu.in>
2026-02-11 17:47:34 +0530

    Add files via upload

---
+++ Grade 8/Volume 1/G8 1.4.2 Conditional Statements with Logical Operators.py
@@ -0,0 +1,33 @@
+# Conditional statements help Python take decisions.
+
+# LOGICAL OPERATORS
+# and → All conditions must be True
+# or  → Any one condition must be True
+# not → Reverses the condition (True becomes False)
+
+# Task 1: Write the code for OR conditions on line 22 onwards.
+# Task 2: Complete the code for NOT conditions on line 31 onwards.
+
+lives = 1
+bonus = False
+
+# Using logical operator AND, check if lives is more than zero and there is no bonus.
+if lives > 0 and bonus == False:          # It is checking both conditions are true.
+    print("You can continue playing.")
+else:
+    print("Game Over!")
+
+extra_life = True
+# Using logical operator OR, check if there are extra_life or if lives is more than zero.
+                                         # It is checking any one condition must be True.
+
+
+
+
+# NOT reverses the condition
+game_over = False
+
+if not game_over:
+                                      # print("Game is running.")
+
+                                       # print("Game stopped.")
