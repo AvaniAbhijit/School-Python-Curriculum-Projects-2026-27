f3235b8c037079989b65cd509914ba6a56abbb13
Avani <avani.abhijit@orchids.edu.in>
2026-02-13 11:14:57 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 1.5 If condition.py
@@ -0,0 +1,13 @@
+# An if condition is used to check a condition.
+# If the condition is True, the code inside the if block runs.
+# If the condition is False, the code does not run.
+
+# Task 1: Change the value of score to 40 on line 8.
+
+
+score = 75
+if score >= 50:            # Check here 75 is greater than 50.
+    print("You Win!")      # This line will execute because above condition is true.
+    print("You unlocked the next level!")
+
+print("Game Over")         # This line will execute always.
