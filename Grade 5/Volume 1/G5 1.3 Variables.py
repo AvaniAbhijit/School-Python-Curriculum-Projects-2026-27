061809e873924ba472da0d7f91f7bfd0b04adc81
Avani <avani.abhijit@orchids.edu.in>
2026-02-13 10:52:02 +0530

    Update G5 1.3 Variables.py

--- Grade 5/Volume 1/G5 1.3 Variables.py
+++ Grade 5/Volume 1/G5 1.3 Variables.py
@@ -3,18 +3,18 @@
 
 # Rules of variables :
 # 1. Start with a letter or _
-#       Correct → age, _score,  Wrong → 2age
+#       Example :  Correct → age, _score,  Wrong → 2age
 
 # 2. No spaces
-#       Correct → student_marks, Wrong → student marks
+#       Example :  Correct → student_marks, Wrong → student marks
 
 # 3. Small and capital letters are different
-#       Age and age are NOT the same.
+#       Example:  Age and age are NOT the same variable.
 
 # 4. No special symbols
-#        Correct → score1, Wrong → score@
+#       Example:  Correct → score1, Wrong → score@
 
-# Task 1: Enter the values for the variable on line 21,24 and 27.
+# Task 1: Enter the values for the variables on lines 21, 24, and 27.
 # Task 2: fix the errors.
 # Task 3: Create a variable on line 31 and print it on line 32.
 
