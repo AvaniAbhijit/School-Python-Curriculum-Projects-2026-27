b334a15c9fbb0b7bac0f523bc916485ceac4a8e1
Avani <avani.abhijit@orchids.edu.in>
2026-02-13 11:50:28 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 1.7 List.py
@@ -0,0 +1,31 @@
+# What is a List?
+# A list is used to store many items in one variable.
+
+# Empty List:
+# A list with nothing inside it.
+
+# List with Strings:
+# When we store words (text) inside a list. Words must be inside quotes.
+
+# Task 1 : Complete the code on line 15,21 and 25.
+# Task 2 : Complete the code from line 27 to 31.
+
+
+fruits = []                           # This is an empty list
+                                      # print empty list.
+fruits = ["Apple", "Banana", "Mango"] # This a list with 3 fruits.
+print(fruits)                         # It will print "Apple", "Banana", "Mango".
+
+# append() Method: is used to add a new item to the list.
+fruits.append("Orange") # Adds Orange to the list
+                        # print the list to see the updated list.
+
+# remove() Method: is used to delete an item from the list.
+fruits.remove("Banana") # Removes Banana from the list
+                        # print the list to see the updated list.
+
+                         # Create empty list name toys.
+#toys._________("Car")   # Add "Car" to the toys list using append().
+#toys._________("Doll")  # Add "Doll" to the toys list.
+#toys._________("Car")   # Remove "Car" from the list using remove().
+#print(_________)        # Print the final toys list.
