d533cd8c497a5e5a28db736a63595b1451703a64
Avani <avani.abhijit@orchids.edu.in>
2026-02-13 15:28:52 +0530

    Add files via upload

---
+++ Grade 7/Volume 1/G7 1.3.1 Conditional Statements with Comparison Operators.py
@@ -0,0 +1,28 @@
+# Conditional statements: Python decides which code to run based on whether a condition is True or False.
+# Operators: Symbols used to compare values or combine conditions in Python.
+
+# COMPARISON OPERATORS
+# > (Greater than), < (Less than), >=  (Greater than or equal to), <=  (Less than or equal to)
+# ==  (Equal to), !=  (Not equal to)
+
+# Task 1: Run the code and check the output.
+# Task 2: Uncomment lines 24 & 26. Complete the code on lines 24 to 27.
+
+# Simple if-else condition - This checks if the age is 13 or more
+age = 14
+if age >= 13:
+    print("You can play this game.")
+else:
+    print("You are too young to play.")
+
+
+# if-elif-else (Multiple conditions) - This checks performance based on score
+score = 75
+if score >= 90:
+    print("Excellent Performance")
+#elif_______:                         # Check if score more than 70
+                                      # print Good Performance
+#else:                                # print Need more practice
+
+else:
+    print("Game stopped.")
