3c08ce61515563583a25bea6a6ee0743f9b0e491
Avani <avani.abhijit@orchids.edu.in>
2026-02-13 12:26:58 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 1.9.2 Return statement.py
@@ -0,0 +1,22 @@
+# What is return?
+# return sends the result back from the function to where it was called.
+
+# Task 1 :  Create a function called multiply_numbers
+#           It should multiply two numbers and return the answer.
+
+
+def add_scores(score1, score2):   # Function with two parameters
+    total = score1 + score2       # Add both numbers and store in total
+    return total                  # Send the total back to where function was called
+
+
+final_score = add_scores(10, 20)  # Calling the function and storing returned value
+print("Total Score is:", final_score)   # Printing the final result
+
+
+                                    # create a function multiply_numbers with two parameters
+                                    # multiply both numbers and store in total
+                                    # Send the total back to where function was called
+
+                                    # Calling the function and storing returned value
+                                    # Printing the final result
