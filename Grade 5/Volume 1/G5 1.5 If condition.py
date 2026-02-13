20464b4b61d7befb7c569388a201aad4a9103b08
Avani <avani.abhijit@orchids.edu.in>
2026-02-13 11:26:05 +0530

    Update G5 1.5 If condition.py

--- Grade 5/Volume 1/G5 1.5 If condition.py
+++ Grade 5/Volume 1/G5 1.5 If condition.py
@@ -2,12 +2,15 @@
 # If the condition is True, the code inside the if block runs.
 # If the condition is False, the code does not run.
 
-# Task 1: Change the value of score to 40 on line 8.
+# What is Indentation?
+# Indentation means giving space before a line to show it belongs inside a block.
+# On line 13,14 has a space before the line, hence it belongs to the if block.
 
+# Task 1: Change the value of score to 40 on line 10.
 
 score = 75
 if score >= 50:            # Check here 75 is greater than 50.
-    print("You Win!")      # This line will execute because above condition is true.
+    print("You Win!")      # This line will execute because the above condition is true.
     print("You unlocked the next level!")
 
 print("Game Over")         # This line will execute always.
