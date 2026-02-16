d94d9c013e67adc8814314b7d8fdaecb025ad0b8
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 16:06:51 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 1.3 Comment.py
@@ -0,0 +1,10 @@
+# What is a comment?
+# A comment starts with #
+# Comments are only for reading the instructions.The computer does NOT run comments.
+
+# Task 1: Remove the # from line 8 and 9 and run the code.(This is called uncomment.).
+# Task 2: Do subtraction (-) and division(/) on line 9 and 10,then run the code.
+
+# Python can also do math.
+#print(5 + 3)
+#print(5 * 3)
