327fef77ac8691f4d6ce5d827354a22cf2d542ab
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 16:04:29 +0530

    Update G5 1.2 print and comment.py

--- Grade 5/Volume 1/G5 1.2 print and comment.py
+++ Grade 5/Volume 1/G5 1.2 print and comment.py
@@ -1,10 +1,10 @@
 # What is Python?
-# Python is a computer language.We use Python to tell the computer what to do.
+# Python is a computer language. We use Python to tell the computer what to do.
 
 # What is print()?
 # print() shows words or numbers on the screen.
 
-# Task 1: Print all your details like, Name,Age and Grade.
+# Task 1: Print all your details, like Name, Age, and Grade.
 
 print("Hello students!")  # This prints one line.
 
