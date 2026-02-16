6820752bbe04b57d56ad2aeef62f8625a4909bb4
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 17:24:26 +0530

    Update G4 2.1.2 Comment.py

--- Grade 4/Volume 1/G4 2.1.2 Comment.py
+++ Grade 4/Volume 1/G4 2.1.2 Comment.py
@@ -1,9 +1,9 @@
 # What is a comment?
 # A comment starts with #
-# Comments are only for reading the instructions.The computer does NOT run comments.
+# Comments are only for reading the instructions. The computer does NOT run comments.
 
-# Task 1: Remove the # from line 8 and 9 and run the code.(This is called uncomment.).
-# Task 2: Do subtraction (-) and division(/) on line 9 and 10,then run the code.
+# Task 1: Remove the # from line 9 and 10 and run the code.(This is called uncomment.).
+# Task 2: Do subtraction (-) and division(/) on lines 9 and 10 ,then run the code.
 
 # Python can also do math.
 #print(5 + 3)
