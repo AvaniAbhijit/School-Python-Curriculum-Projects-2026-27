e7e31498989c810c30d89527be2c050675c2ba73
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 15:53:38 +0530

    Update G6 2.8 Global Variable.py

--- Grade 6/Volume 1/G6 2.8 Global Variable.py
+++ Grade 6/Volume 1/G6 2.8 Global Variable.py
@@ -7,27 +7,23 @@
 # 2. When we want to share data across the whole program.
 # 3. It helps avoid creating the same variable repeatedly.
 
-# Task 1: Call the function on line 20  and print its output on line 22.
-# Task 2: Assign a different value to the score variable on line 27 
-#         and call the function on line 30.
+# Task 1: Call the function on line 18  and print its output on line 19.
+# Task 2: Assign a different value to the score variable on line 25 
+#         and call the function on line 26.
 
-# Create a global variable
-score = 100
+score = 100                                 # Create a global variable
 
-# Use it inside a function
-def show_score():
+def show_score():                           # Use it inside a function
     print("Current Score is:", score)
-#Call the function
-
-# Print it outside the function
+                                            # Call the function
+                                            # Print it outside the function
 
 
 # Change the global variable inside the function
 def update_score():
     global score
-   # Assign a different value to the score variable.
-
-#Call the function
+                                            # Assign a different value to the score variable.
+                                            #Call the function
 
 # Print updated value
 print("Updated Score:", score)
