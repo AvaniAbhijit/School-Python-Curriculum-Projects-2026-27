11be297a3551b5f245c6e97416ea376d94a02b90
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 12:54:41 +0530

    Add files via upload

---
+++ Grade 4/Volume 1/G4 2.4  Data Type.py
@@ -0,0 +1,18 @@
+# What are data types?
+# Data type means what kind of value we are storing.
+# It tells us if the value is:Text, Number
+
+# Task 1 : Complete the code on lines 13.
+
+name = "Rahul" #name is a variable and "rahul" is a value which is string data type.
+print(name)
+print(type(name))  #This will check the data type.
+
+age = 10      #age is a variable and 10 is a value which is integer data type.
+print(age)
+                   #This will check the data type.
+
+
+
+
+
