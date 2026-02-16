aee66e42b6178d2e5e50cf596cb8e1e4d571f924
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 11:04:42 +0530

    Update G8 1.4.1 For Loop.py

--- Grade 8/Volume 1/G8 1.4.1 For Loop.py
+++ Grade 8/Volume 1/G8 1.4.1 For Loop.py
@@ -2,14 +2,9 @@
 # The for loop is used when you know how many times to repeat the instructions.
 # Observe the indentation of code within the for loop.
 
-# Task 1: Print numbers from 120 to 145.
-# Task 2: Uncomment lines no. 13 & 14, run the code, and observe the output.
-# Task 3: Change the "Orchids" string to numbers 123 on line 14. 
+# Task 1: Print numbers from 120 to 145 using range.
+ 
 
 # repeats 10 times from 1 to 10. The last number in a range is not included.
 for num in range(1, 11):
     print(num)
-
-# repeats based on the number of letters in the string - Orchids
-#for letter in "Orchids":
-#    print(letter)
