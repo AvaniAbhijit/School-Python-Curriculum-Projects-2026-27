a1533c8504cc63d4647d8e5a5a539daf9821d228
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 11:13:49 +0530

    Add files via upload

---
+++ Grade 8/Volume 1/G8 1.4.2 For loop with string.py
@@ -0,0 +1,9 @@
+# A for loop looks at each letter in a word one by one.
+
+# Task 1 : Change the word to Orchid School on line 5.
+
+word = "Hello"        # This is a string (text)
+
+# Loop through each letter in the word
+for letter in word:
+    print(letter)     # Print the current letter
