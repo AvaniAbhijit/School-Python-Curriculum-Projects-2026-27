d1742c2ae69c82d8cf9b08853a449b67487cc94e
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 15:37:53 +0530

    Update G6 1.7.3 if else practice problem statement.py

---
+++ Grade 6/Volume 1/G6 1.7.3 if else practice problem statement.py
@@ -0,0 +1,8 @@
+# Task - Write if-else statement to check the number entered by user is even or odd.
+
+# Hint to solve problem - 
+# 1. Use input() to take a number from the user.
+# 2. Convert the input to an integer using int().
+# 3. Use the modulus operator % to check the remainder when dividing by 2.
+# 4. If the remainder is 0, the number is even.
+# 5. Otherwise, the number is odd.
