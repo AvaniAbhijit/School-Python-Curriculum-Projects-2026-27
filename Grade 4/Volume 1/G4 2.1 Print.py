c4ca7394737f271caeff0d58d1dddbd8b5c4c57b
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 12:41:56 +0530

    Add files via upload

---
+++ Grade 4/Volume 1/G4 2.1 Print.py
@@ -0,0 +1,14 @@
+# What is Python?
+# Python is a computer language.We use Python to tell the computer what to do.
+
+# What is print()?
+# print() shows words or numbers on the screen.
+
+# Task 1: Print all your details like, Name,Age and Grade.
+
+print("Hello students!")  # This prints one line.
+
+# This prints more than one line.
+print("Welcome to Python class.")
+print("Python is fun!")
+
