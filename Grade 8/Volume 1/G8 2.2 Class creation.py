314cba4f39747b5c4b5d085b5416dcc18e262d23
Avani <avani.abhijit@orchids.edu.in>
2026-02-16 11:52:03 +0530

    Add files via upload

---
+++ Grade 8/Volume 1/G8 2.2 Class creation.py
@@ -0,0 +1,15 @@
+# What is a class?
+# A class is a design or blueprint.
+
+# What is a function in the class?
+# A function is an action or task.It tells what the class can do.
+
+# Task 1 : Create Your Own Class - Create a class named Juice - Print “Fresh Juice Ready!”
+
+
+class Pizza:  # Create a class named Pizza (this is a blueprint)
+
+    def show_message():                  # Define a function inside the class
+        print("Welcome to Pizza Shop!")  # This line prints a message
+
+Pizza.show_message()                     # Call the function using the class name
