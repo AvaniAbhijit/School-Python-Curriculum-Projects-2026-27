558665e46dfa42b7968c63a5f7f532aaef936a0c
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 16:43:59 +0530

    Add files via upload

---
+++ Grade 7/Volume 1/G7 1.6.1 append with list.py
@@ -0,0 +1,27 @@
+# What is a List?
+# A list is used to store many items in one variable.
+
+# Empty List:
+# A list with nothing inside it.
+
+# List with Strings:
+# When we store words (text) inside a list. Words must be inside quotes.
+
+# Task 1 : Complete the code on line 15,21 and 24.
+# Task 2 : Uncomment and Complete the code from line 25 to 27.
+
+
+fruits = []                           # This is an empty list
+                                      # print empty list.
+fruits = ["Apple", "Banana", "Mango"] # This a list with 3 fruits.
+print(fruits)                         # It will print "Apple", "Banana", "Mango".
+
+# append() Method: is used to add a new item to the list.
+fruits.append("Orange") # Adds Orange to the list
+                        # print the list to see the updated list.
+
+
+                         # Create empty list name games.
+#games._________("Mario")   # Add "Mario" to the games list using append().
+#games._________("Candycrush")  # Add "Candycrush" to the games list.
+#print(_________)        # Print the final games list.
---
+++ Grade 7/Volume 1/G7 1.6.2 remove with list.py
@@ -0,0 +1,15 @@
+
+# Task 1 : Complete the code on line 11 and 13.
+# Task 2 : Uncomment and Complete the code from line 14 to 15.
+
+
+fruits = ["Apple", "Banana", "Mango"] # This a list with 3 fruits.
+print(fruits)                         # It will print "Apple","Banana","Mango".
+
+# remove() Method: is used to delete an item from the list.
+fruits.remove("Banana") # Removes Banana from the list
+                        # print the list to see the updated list
+
+                        # Create a list of games with names of different games.
+#games._________("")    # Remove 1 game from the list using remove().
+#print(_________)       # Print the final games list.
