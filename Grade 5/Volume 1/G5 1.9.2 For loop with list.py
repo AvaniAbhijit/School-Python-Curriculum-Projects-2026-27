082cc0f223a9e11535eaa379035ddb36c6eb075d
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 10:31:59 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 1.9.2 For loop with list.py
@@ -0,0 +1,14 @@
+# A for loop repeats an action for every item in the list.
+# The loop picks one item → runs the code → then moves to the next item.
+
+# Task 1 : Complete the code from line 11 to 13.
+
+fruits = ["apple", "banana", "mango", "orange"] # This is a list of fruits
+
+for fruit in fruits:       # This for loop goes through each fruit in the list one by one.
+    print("I like", fruit) # This prints a sentence using the current fruit
+
+                            # Create toys list with different names of toys
+                            # Write for loop to iterate through the toys list.
+                            # Print each toy.
+
