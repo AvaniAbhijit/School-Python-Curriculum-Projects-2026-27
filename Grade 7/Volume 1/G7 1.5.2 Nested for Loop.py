961e50a0abb0c5ff6e22980d75f085385e06a744
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 16:32:49 +0530

    Add files via upload

---
+++ Grade 7/Volume 1/G7 1.5.2 Nested for Loop.py
@@ -0,0 +1,6 @@
+# Print numbers from 120 to 145 using nested loops
+
+for num in range(1, 5):     # Outer loop
+    print(num)
+    #for i in range(1, 3):       # Inner loop (runs 2 times for each num)
+    #   print("Number:", num, "Repeat:", i)
