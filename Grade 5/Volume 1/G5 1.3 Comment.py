f2b649fe1ee429d76be877eaeeacc9ba37737df6
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 10:05:55 +0530

    Update G5 1.3 Comment.py

--- Grade 5/Volume 1/G5 1.3 Comment.py
+++ Grade 5/Volume 1/G5 1.3 Comment.py
@@ -2,7 +2,7 @@
 # A comment starts with #
 # Comments are only for reading the instructions.The computer does NOT run comments.
 
-# Task 1: Remove the # from line 8 and 9 and run the code.(This is called uncomment.).
+# Task 1: Remove the # from line 9 and 10 and run the code.(This is called uncomment.).
 # Task 2: Do subtraction (-) and division(/) on line 9 and 10,then run the code.
 
 # Python can also do math.
