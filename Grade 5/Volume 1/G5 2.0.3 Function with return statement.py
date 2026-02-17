579e7ce93056b4eda1a4352b497bc5efc11bc1a1
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 10:58:17 +0530

    Update and rename G5 1.9.2 Return statement.py to G5 2.0.3 Function with return statement.py

--- Grade 5/Volume 1/G5 2.0.3 Function with return statement.py
+++ Grade 5/Volume 1/G5 2.0.3 Function with return statement.py
@@ -1,13 +1,13 @@
 # What is return?
 # return sends the result back from the function to where it was called.
 
-# Task 1 :  Create a function called multiply_numbers
+# Task 1:  Create a function called multiply_numbers
 #           It should multiply two numbers and return the answer.
 
 
 def add_scores(score1, score2):   # Function with two parameters
     total = score1 + score2       # Add both numbers and store in total
-    return total                  # Send the total back to where function was called
+    return total                  # Send the total back to where the function was called
 
 
 final_score = add_scores(10, 20)  # Calling the function and storing returned value
