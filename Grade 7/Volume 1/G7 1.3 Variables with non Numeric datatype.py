96eeaf37d8ee55513e1025c3f158081965bc0c9c
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 14:43:11 +0530

    Update G7 1.3 Variables with non Numeric datatype.py

--- Grade 7/Volume 1/G7 1.3 Variables with non Numeric datatype.py
+++ Grade 7/Volume 1/G7 1.3 Variables with non Numeric datatype.py
@@ -1,48 +1,21 @@
-# Variables are used to store information in a program.
-# The value of a variable can be changed while the program is running.
-# Data types tell Python what kind of value is stored in a variable, such as a number,
-# text, or True/False.
+# Rules of variables :
+# 3. Variable names are case-sensitive.
+#       Age and age are treated as different variables.
+# 4. Numbers and special symbols are not allowed in variable names.
+#       Only letters, numbers (not at start), and underscore (_) are allowed.
+
+# Data types tell Python what kind of value is stored in a variable, such as text, or True/False.
 
 
-# Task 1: Run the code and check the output.
-# Task 2: Change the values of the variables.
-# Task 3: Create one new variable of your choice and print it.
-
-name = "Aarav"
-age = 13
-
-print(name)
-print(age)
-
+# Task 1: Correct the variable name on lines 14,15 and 19 as per the variable rule.
+# Task 2: Complete the code on lines 16,20 and 21.
 
 # 1. String (text data)
-school = "Green Valley School"
-print(school)
+Name = "Green Valley School" # school is a variable here, and "Green Valley School" is a value.
+print(name)                  # print variable value.
+                             # print variable data type.
 
-
-# 2. Integer (whole numbers)
-marks = 85
-print(marks)
-
-
-# 3. Float (decimal numbers)
-height = 5.6
-print(height)
-
-
-# 4. Boolean (True or False)
-is_student = True
-print(is_student)
-
-print(type(name))
-print(type(age))
-print(type(height))
-print(type(is_student))
-
-a = 10
-b = 5
-
-print(a + b)   # Addition
-print(a - b)   # Subtraction
-print(a * b)   # Multiplication
-print(a / b)   # Division
+# 2. Boolean (True or False)
+$condtion = True             # variable that has an integer value.
+                              # print variable value.
+                              # print variable data type.
