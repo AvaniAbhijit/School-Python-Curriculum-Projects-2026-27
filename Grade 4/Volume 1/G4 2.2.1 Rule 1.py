5f503dd889357e67d42dee098b8f08a3e8efbe0c
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 15:05:20 +0530

    Update G4 2.2.1 Rule 1.py

--- Grade 4/Volume 1/G4 2.2.1 Rule 1.py
+++ Grade 4/Volume 1/G4 2.2.1 Rule 1.py
@@ -1,12 +1,13 @@
 # What is a variable?
-# A variable is like a box.We use it to store value.
+# A variable is like a box. We use it to store value.
 # The value inside the variable can change.
 
-# Rule 1: A variable name must start with a letter.It cannot start with a number.
+# Rule 1: A variable name must start with a letter. It cannot start with a number.
 
-# Task 1: Remove # in lines 12 & 13 , run the code and fix the error.:
+# Task 1: Remove # in lines 13, run the code, and fix the error.
+# Task 2: Print the variable on line 14.
 
-marks = 90   #marks is the variable name ,90 is the value.
+marks = 90   #marks is the variable name,90 is the value.
 print(marks)
 
 #1name = "rahul"
