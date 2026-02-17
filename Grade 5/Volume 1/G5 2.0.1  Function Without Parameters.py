e7923d7163bf15d66618a507cddffd2e26d37753
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 10:56:04 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 2.0.1  Function Without Parameters.py
@@ -0,0 +1,21 @@
+# What is a function?
+# A function is a small set of instructions that does a job.
+# We use functions so we don’t repeat the same code again and again.
+
+# Task 1 : Define function show_rules() on line 16.
+# Task 2 : Move the line 17 & 18 inside the show_rules() function.
+# Task 3: Call the show_rules() function on line 20.
+
+
+def start_game():                       # This function starts the game.
+    print("Game Started!")
+    print("Get ready to boost your power")
+
+start_game()                            # Calling the function
+
+                                        # Create a new function name show_rules().
+print("Rule 1: Be fast")
+print("Rule 2: Have fun")
+
+                                        #  Call the  function show_rules().
+
---
+++ Grade 5/Volume 1/G5 2.0.2 Function with parameter.py
@@ -0,0 +1,21 @@
+# What is a parameter?
+# A parameter is a value you give to a function so it can use it.
+
+# Task 1 : Define function show_rules() with parameter game_name on line 15 .
+# Task 2 : Uncomment and move the line 16,17 & 18 inside the show_rules() function.
+# Task 3: Call the show_rules() function with any game name as a parameter on line 1.9
+
+def start_game(player_name):      # start_game is function with player_name is a parameter.
+    print("Game Started!")
+    print("Welcome", player_name)
+    print("Get ready to boost your power")
+
+start_game("Alex")                # Call the function and pass a name as a parameter.    
+
+                                  # Create a function with game_name as the parameter.
+#print("Rules of", game_name)
+#print("Rule 1: Be fast")
+#print("Rule 2: Have fun")
+                                  # Call the function with parameter game_name.
+
+
