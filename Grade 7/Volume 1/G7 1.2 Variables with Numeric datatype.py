5c47617f67eed6a117e498bde2958a34f00f3569
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 14:32:35 +0530

    Update and rename G7 1.2 Variables & Data Types.py to G7 1.2 Variables with Numeric datatype.py

---
+++ Grade 7/Volume 1/G7 1.2 Variables with Numeric datatype.py
@@ -0,0 +1,25 @@
+# Variables are used to store information in a program. 
+# The value of a variable can be changed while the program is running.
+
+# Rules of variables :
+# 1. Variable name must start with a letter or underscore (_).
+#       It cannot start with a number.
+# 2. Variable names should not contain spaces.
+#       Use underscore (_) instead of space.
+
+# Data types tell Python what kind of value is stored in a variable, such as a number or a decimal.
+
+# Task 1: Correct the variable name on lines 16,18 and 21 as per the variable rule.
+# Task 2: Complete the code on lines 17,22 and 23
+
+# 1. Integer (whole numbers)
+1age = 12                     # variable that has an integer value.
+                              # print variable value.
+print(type(1age))             # print variable data type.
+
+# 2. Float (decimal numbers)
+Body weight = 56.5            # variable that has a float value.
+                              # print variable value.
+                              # print variable data type.
+
+
