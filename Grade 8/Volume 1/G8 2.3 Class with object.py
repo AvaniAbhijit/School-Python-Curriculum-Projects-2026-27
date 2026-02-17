4e92cb5eab48263f6452bb6967e55cefeb8df87b
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 11:43:48 +0530

    Update G8 2.3 Class with object.py

--- Grade 8/Volume 1/G8 2.3 Class with object.py
+++ Grade 8/Volume 1/G8 2.3 Class with object.py
@@ -5,16 +5,17 @@
 # self means “this object.”
 
 # Task 1: Check the error and add self as a parameter to show_message() function on line 15.
-# Task 2: Create Your Own Class withan  Object
+# Task 2: Create Your Own Class with an  Object
 #       1. Create a class named Sport
-#       2. Print “Welcome to Sports Academy”
+#       2. Create a function show_message() and Print “Welcome to Sports Academy”
 #       3. Create an object and call the function
 
 class Pizza:                             # Create a class named Pizza (this is a blueprint)
 
-    def show_message():                  # Define a function inside the class
+    def show_message(self):                  # Define a function inside the class
         print("Welcome to Pizza Shop!")  # This line prints a message
 
-p1 = Pizza()                             # Create an object from the class
-p1.show_message()                        # Call the function using the object
+p1 = Pizza()                          # Create an object from the class
+p1.show_message()                     # Call the function using the object
                                      
+
