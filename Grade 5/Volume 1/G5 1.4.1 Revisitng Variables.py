3a693098743bce5f9cf57dde7c3bcf33dba5d130
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 14:13:22 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 1.4.1 Revisitng Variables.py
@@ -0,0 +1,5 @@
+# Task 1: Create variables to store:Your name,Your class,Your age
+#         Display all the information clearly on the screen using print().
+
+# Task 2: Store your marks for:Math,Science
+#         Add both marks and display the total marks using print().
