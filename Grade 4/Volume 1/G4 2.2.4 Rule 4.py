a4b6fd5b3483e244e534d6a62278801c00ef5a9a
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 15:07:50 +0530

    Update G4 2.2.4 Rule 4.py

--- Grade 4/Volume 1/G4 2.2.4 Rule 4.py
+++ Grade 4/Volume 1/G4 2.2.4 Rule 4.py
@@ -1,9 +1,10 @@
 # Rule 4: Special symbols are not allowed other than underscore(_) in variable names.
 
-# Task 1: 
+# Task 1: remove # in lines 9 and run the code, and fix the error.
+# Task 2: Print the variable on line 10.
 
 score_1 = 95
 print(score_1)
 
 #score@1 = 95
-print(score@1)
+
