ac552a04a945c0e10cfa2bd3c09ecce6e8e2284f
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 10:16:05 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 1.7.2 List with remove.py
@@ -0,0 +1,15 @@
+
+# Task 1 : Complete the code on line 11 and 13.
+# Task 2 : Uncomment and Complete the code from line 14 to 15.
+
+
+fruits = ["Apple", "Banana", "Mango"] # This a list with 3 fruits.
+print(fruits)                         # It will print "Apple","Banana","Mango".
+
+# remove() Method: is used to delete an item from the list.
+fruits.remove("Banana") # Removes Banana from the list
+                        # print the list to see the updated list
+
+                        # Create a list of toys with names of different toys.
+#toys._________("")     # Remove 1 toy from the list using remove().
+#print(_________)       # Print the final toys list.
