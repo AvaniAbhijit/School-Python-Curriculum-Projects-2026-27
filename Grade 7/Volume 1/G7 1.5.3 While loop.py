a15116ef2507ad88a667786e5d2847bf7dfc35b2
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 16:38:16 +0530

    Add files via upload

---
+++ Grade 7/Volume 1/G7 1.5.3 While loop.py
@@ -0,0 +1,14 @@
+# The while loop is used when you want to repeat instructions until a condition is false.
+
+# Task 1: Complete the code on lines 7 & 8.
+# Task 2: Complete the code on lines 12 & 13.
+
+
+                                 # Create a variable running and assign True.
+                                 # Create a variable num and assign 0.
+while running:
+    num = num + 5
+    print(num, " in while loop")
+                                 # Write an if condition to check if num is greater than 20.
+                                 # set the Running variable to false.
+print("done running while loop")
