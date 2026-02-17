b7a3e77debb09429fb0079cb9fe04fff24396a95
Avani <avani.abhijit@orchids.edu.in>
2026-02-17 16:56:29 +0530

    Add files via upload

---
+++ Grade 7/Volume 1/G7 1.8 Function.py
@@ -0,0 +1,28 @@
+# A function is a block of code that performs a specific task.
+# We use functions to avoid repeating the same code again and again.
+# 1. def is used to create a function.
+# 2. A function without parameters does not take input.
+# 3. Function witha  parameter takesa  value while calling.
+
+# Task 1: Call the function show_power by passing the value 30 on line 21.
+# Task 2: Uncomment and complete the code from line 24 onwards.
+
+# Function without parameter
+def start_game():
+    print("Game Started!")
+    print("Get ready to boost your power")
+print(start_game())                         # start_game function called here.
+
+# Function with parameter
+def show_power(power):
+    print("Your current power is:", power)
+
+# Calling the function with a parameter
+
+
+# Create a new function to add 2 numbers
+#def __________():
+#    ___________________________
+
+# Call the new function
+#_________________
