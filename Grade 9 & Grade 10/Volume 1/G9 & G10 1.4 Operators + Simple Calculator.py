decfb546b832d0f4974fbc34a23eabb8a7235102
Avani <avani.abhijit@orchids.edu.in>
2026-02-18 17:00:13 +0530

    Add files via upload

---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.4 Operators + Simple Calculator.py
@@ -0,0 +1,21 @@
+# Arithmetic operators are used to perform mathematical calculations on numbers.
+#           Operator	                     Example
+#       + (Addition)	                     5 + 3 → 8
+#       -	Subtraction	                     5 - 3 → 2
+#       *	Multiplication	                 5 * 3 → 15
+#       /	Division	                     10 / 2 → 5.0
+#       //	Floor Division (no decimal)	     10 // 3 → 3
+#       %	Modulus (remainder)	             10 % 3 → 1
+#       **	Power	                         2 ** 3 → 8
+
+
+# Task : Build a mini caluclator, which will 
+#        1. Accepts two number from the User.
+#        2. Perfome following operations on the two numbers -
+#           2.1 Addition 
+#           2.2 Subtraction
+#           2.3 Multiplication
+#           2.4 Division
+#           2.5 Divisibility 
+
+
