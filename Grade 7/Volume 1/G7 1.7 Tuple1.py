531e9bc2136adbd6084ca008738cb2f89984935c
Avani <avani.abhijit@orchids.edu.in>
2026-02-18 13:30:10 +0530

    Add files via upload

---
+++ Grade 7/Volume 1/G7 1.7 Tuple1.py
@@ -0,0 +1,20 @@
+# What is a Tuple?
+# A tuple is used to store multiple values in one variable.
+# A tuple is written using round brackets( ).
+
+# Task 1: Print the length of the tuple on a line on line 14.
+# Task 2: Print the first item of the tuple using index on line 17.
+# Task 3: Uncomment line 20 and print the tuple and solve the error.
+
+# Creating a Tuple
+num = (11,55,66,33)
+print(num)
+
+# print length of a Tuple
+
+
+# Accessing Items using Index - starts from 0
+
+
+# Tuples cannot be changed. We cannot add or remove items from a tuple.
+# num.append(99)
