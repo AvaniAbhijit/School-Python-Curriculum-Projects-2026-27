7d5eb9e9d350b866e53d69ad001b49dc71101dd1
Avani <avani.abhijit@orchids.edu.in>
2026-02-18 16:49:44 +0530

    Update G9 & G10 1.3.2 Type Conversion.py

---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.3.2 Type Conversion.py
@@ -0,0 +1,11 @@
+# Why int() is important
+# To do math, we must convert text into numbers.
+
+# Task 1: Write code to subtract two float numbers from line 12 onwards.
+
+
+# Convert user input into integer
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum:", num1 + num2)         # Now Python performs real addition
