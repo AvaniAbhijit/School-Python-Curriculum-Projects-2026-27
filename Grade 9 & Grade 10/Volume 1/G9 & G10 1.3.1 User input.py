5bd4c0a28126034e1158eac42dc3c06d3e4bb04a
Avani <avani.abhijit@orchids.edu.in>
2026-02-18 16:48:50 +0530

    Add files via upload

---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.3.1 User input.py
@@ -0,0 +1,12 @@
+# input() ALWAYS RETURNS STRING :
+# When a user types something, Python stores it as text, even if it looks like a number.
+
+# Task 1: Complete the code from line 10 to 12.
+
+name = input("Enter your name: ")  # input() stores user data as STRING (text)
+print("Hello", name)               # The value is stored exactly as typed
+print(type(name))                  # Show the type of data stored
+
+                    # Take number input from the user and store in num1 variable.
+                    # Take number input from the user and store in num2 variable.
+                    # print the sum of num1 and num2
---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.3.3 Practice.py
@@ -0,0 +1,5 @@
+# Create a program that:
+#   1. Ask your name :
+#   2. Asks 3 subject marks
+#   3. Calculates average
+#   4. Prints result
