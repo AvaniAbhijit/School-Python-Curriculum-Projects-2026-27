d9f58b28df23abcff3d1faeee405cafa669660ab
Avani <avani.abhijit@orchids.edu.in>
2026-02-18 16:18:12 +0530

    Update G9 & G10 1.2.1 Practice 1.py

---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.2.1 Practice 1.py
@@ -0,0 +1,11 @@
+# Create a Game Player Profile program that stores:
+# 1. player name (string)
+# 2. level (integer)
+# 3. health percentage (float)
+# 4. is_alive (boolean based on health > 0)
+# 5. Print a formatted report like:
+#     Player Report
+#     Name: ______
+#     Level: ______
+#     Health: ______
+#     Alive: ______
