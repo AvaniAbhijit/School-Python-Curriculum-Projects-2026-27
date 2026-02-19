86220a3946627f365c85d8497177d1f887db4739
Avani <avani.abhijit@orchids.edu.in>
2026-02-19 09:55:57 +0530

    Update G7 1.5.4 Practice on while.py

--- Grade 7/Volume 1/G7 1.5.4 Practice on while.py
+++ Grade 7/Volume 1/G7 1.5.4 Practice on while.py
@@ -1,2 +1,3 @@
-# Task  — Even or Odd from 1 to 10 using while loop and if/else.
-# Print numbers from 1 to 10 and show whether each is Even or Odd.
+# Task  — Even or Odd from 1 to 10 using a while loop and if/else.
+# Print numbers from 1 to 10 and show whether each is even or Odd.
+# Hint: use % 2 to check number is Even or Odd.
