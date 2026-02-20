cd961559074f398b4bdeaac01b341d66b278f108
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 10:33:10 +0530

    Add files via upload

---
+++ Grade 7/Volume 1/G7 1.4.3 Conditional statemnet with Not operator.py
@@ -0,0 +1,16 @@
+# The NOT operator reverses a condition’s truth value — 
+# it turns true into false and false into true.
+
+# Task : Write a code to check the following on line 17 onwards -
+#        1. Create a variable is_raining = True.
+#        2. Use the not operator to check if it is not raining.
+#        3. If it is not raining → print "You can play outside!"
+#        4. Otherwise → print "Take an umbrella!"
+
+
+game_over = False
+
+if not game_over:
+    print("Game is running.")         # print("Game is running.")
+else:
+    print("Game stopped.")            # print("Game stopped.")
