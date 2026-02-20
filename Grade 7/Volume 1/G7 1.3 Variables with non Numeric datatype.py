907cd78cb72f02b04a1ed35c902a1000f720ed35
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 09:55:49 +0530

    Update G7 1.3 Variables with non Numeric datatype.py

--- Grade 7/Volume 1/G7 1.3 Variables with non Numeric datatype.py
+++ Grade 7/Volume 1/G7 1.3 Variables with non Numeric datatype.py
@@ -4,11 +4,12 @@
 # 4. Numbers and special symbols are not allowed in variable names.
 #       Only letters, numbers (not at start), and underscore (_) are allowed.
 
-# Data types tell Python what kind of value is stored in a variable, such as text, or True/False.
+# Data type tells Python what kind of value is stored in a variable, such as text, or True/False.
 
-
-# Task 1: Correct the variable name on lines 14,15 and 19 as per the variable rule.
-# Task 2: Complete the code on lines 16,20 and 21.
+# Task 1: Run the code and check the error.
+# Task 2: Correct the variable name on lines 15 or 16 as per the rule and complete the code on line 17.
+# Task 3: Uncomment the code on line 20 and run the code to check the error.
+# Task 4: Correct the variable name on line 20 as per the rule and complete the code on lines 21,22.
 
 # 1. String (text data)
 Name = "Green Valley School" # school is a variable here, and "Green Valley School" is a value.
@@ -16,6 +17,6 @@ print(name)                  # print variable value.
                              # print variable data type.
 
 # 2. Boolean (True or False)
-$condtion = True             # variable that has an integer value.
+#$condtion = True             # variable that has an integer value.
                               # print variable value.
                               # print variable data type.
