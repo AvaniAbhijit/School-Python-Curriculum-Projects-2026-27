25a32ec7bc3542dc8e340562b6fec4f0da816c9c
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 12:44:43 +0530

    Add files via upload

---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 Mini Project.py
@@ -0,0 +1,7 @@
+# Mini Project - Create a quiz based on the following rules - 
+#      Program checks answers, gives score, and shows performance level.
+#       1. Ask student name
+#       2. Ask 3 questions
+#       3. Give 2 attempts per question
+#       4. Calculate total score
+#       5. Show performance result
