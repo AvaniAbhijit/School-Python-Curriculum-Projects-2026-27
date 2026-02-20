f18040c7e513b613c4ede80e0205f80df3210da1
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 10:35:02 +0530

    Update G7 1.5.2 Nested for Loop.py

--- Grade 7/Volume 1/G7 1.5.2 Nested for Loop.py
+++ Grade 7/Volume 1/G7 1.5.2 Nested for Loop.py
@@ -1,7 +1,7 @@
-# Nested for loop means one loop runs inside another loop, 
+# Nested for loop means one loop runs inside another loop,
 # so the inner loop repeats fully for each outer loop value.
 
-# Task 1: Uncomment the inner loop on line 11, & 12.
+# Task 1: Uncomment the inner loop on lines 11 & 12 and observe the output.
 # Task 2: Uncomment the line 13 and see the change.
 # Task 3: Change the range for outer for loop.
 
@@ -10,7 +10,4 @@ for row in range(1, 6):        # Outer loop controls rows
 
     #for star in range(0,row):    # Inner loop prints stars
     #   print("*", end=" ")
-    #print() 
-
-    
-
+    #print()
