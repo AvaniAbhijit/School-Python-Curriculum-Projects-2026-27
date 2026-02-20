c70b2340dc850fffd77afb31354342a0973d10da
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 10:48:32 +0530

    Update G9 & G10 1.2.1 Practice 1.py

--- Grade 9 & Grade 10/Volume 1/G9 & G10 1.2.1 Practice 1.py
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.2.1 Practice 1.py
@@ -3,9 +3,10 @@
 # 2. level (integer)
 # 3. health percentage (float)
 # 4. is_alive (boolean based on health > 0)
-# 5. Print a formatted report like:
+# 5. Print a formatted report like below:
 #     Player Report
 #     Name: ______
 #     Level: ______
 #     Health: ______
 #     Alive: ______
+
