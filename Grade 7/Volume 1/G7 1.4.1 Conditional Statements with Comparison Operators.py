1d9ad338864cd32abcd8a56523f89a18348edf17
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 10:11:00 +0530

    Update G7 1.4.1 Conditional Statements with Comparison Operators.py

--- Grade 7/Volume 1/G7 1.4.1 Conditional Statements with Comparison Operators.py
+++ Grade 7/Volume 1/G7 1.4.1 Conditional Statements with Comparison Operators.py
@@ -3,17 +3,19 @@
 # > (Greater than), < (Less than), >=  (Greater than or equal to), <=  (Less than or equal to)
 # ==  (Equal to), !=  (Not equal to)
 
-# Task 1: Run the code and check the output. Observe the indentation on lines 14 & 16.
-# Task 2: Change the age to 10 on line 12 and run the code to observe the output.
-# Task 3: Uncomment lines 20 to 22. Run and see the output.
-# Task 4: Uncomment lines 23 & 25. Complete the code on lines 23 to 26 to check for one more condition.
+# Note: Indentation is the spacing at the beginning of a line of code to show structure.
+
+# Task 1: Run the code and check the output. Observe the indentation on lines 16 & 18.
+# Task 2: Change the age to 10 on line 14 and run the code to observe the output.
+# Task 3: Uncomment lines 22 to 24. Run and see the output.
+# Task 4: Uncomment & Complete the code on lines 25-27 to add one more condition.
 
 # Simple if-else condition - This checks if the age is 13 or more
 age = 14
 if age >= 13:
-    print("You can play this game.")      # runs if the condition is True
+    print("You can play this game.")      # indented code runs if the condition is True
 else:
-    print("You are too young to play.")   # runs if the condition is False
+    print("You are too young to play.")   # indented code runs if the condition is False
 
 
 # if-elif-else (Multiple conditions) - This checks performance based on score
