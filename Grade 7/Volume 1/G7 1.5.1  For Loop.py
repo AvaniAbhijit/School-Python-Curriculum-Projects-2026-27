979742447f3c6be9c18b0b55cddb7548e6f13d16
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 10:35:55 +0530

    Update G7 1.5.1  For Loop.py

--- Grade 7/Volume 1/G7 1.5.1  For Loop.py
+++ Grade 7/Volume 1/G7 1.5.1  For Loop.py
@@ -2,8 +2,9 @@
 # The for loop is used when you know how many times to repeat the instructions.
 # Observe the indentation of code within the for loop.
 
-# Task 1: Print numbers from 120 to 145 using range.
+# Task 1: Print numbers from 120 to 145 using range on line 11 onwards.
  
 # repeats 10 times from 1 to 10. The last number in a range is not included.
 for num in range(1, 11):
     print(num)
+
