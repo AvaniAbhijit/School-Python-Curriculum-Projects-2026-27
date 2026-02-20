6581b22fd358faa072278c917dcf8612ecdd38c9
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 12:38:34 +0530

    Update G9 & G10 1.9 Nested Loops + Patterns.py

--- Grade 9 & Grade 10/Volume 1/G9 & G10 1.9 Nested Loops + Patterns.py
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.9 Nested Loops + Patterns.py
@@ -1,7 +1,13 @@
+# Nested for loop means one loop runs inside another loop,
+# so the inner loop repeats fully for each outer loop value.
 
-team = ["Ali", "Sara", "John"]
-print("Captain:", team[0])
+# Task 1: Uncomment the inner loop on lines 11 & 12 and observe the output.
+# Task 2: Uncomment the line 13 and see the change.
+# Task 3: Change the range for outer for loop.
 
-# Task: 1. Favorite movies list
-#       2. Print last item
+for row in range(1, 6):        # Outer loop controls rows
+    print(row)
 
+    #for star in range(0,row):    # Inner loop prints stars
+    #   print("*", end=" ")
+    #print()
