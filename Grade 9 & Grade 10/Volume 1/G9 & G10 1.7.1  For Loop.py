06d1ea146125288cd9173f18939ea0639e6750da
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 11:25:48 +0530

    Add files via upload

---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.7.1  For Loop.py
@@ -0,0 +1,14 @@
+# A for loop repeats instructions a fixed number of times.
+# range() provides the sequence of numbers the loop follows.
+
+# Task 1: Write a for loop using range() to print on line 16 onwards:
+#          1. "Mission 1 Started" to "Mission 5 Started"
+#               After the loop, print:
+#          2. "All Missions Activated!"
+
+
+# The game unlocks levels one by one.
+for level in range(1, 6):
+    print("Level", level, "Unlocked")     # Unlocks Level 1 to 5
+
+print("All Levels Ready!")
