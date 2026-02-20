81ac1a5c98f89afa0a400b2639fbf673ee8079ed
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 09:58:52 +0530

    Update G7 1.4.1 Conditional Statements with Comparison Operators.py

--- Grade 7/Volume 1/G7 1.4.1 Conditional Statements with Comparison Operators.py
+++ Grade 7/Volume 1/G7 1.4.1 Conditional Statements with Comparison Operators.py
@@ -3,8 +3,8 @@
 # > (Greater than), < (Less than), >=  (Greater than or equal to), <=  (Less than or equal to)
 # ==  (Equal to), !=  (Not equal to)
 
-# Task 1: Run the code and check the output. Observe the indentation.
-# Task 2: Change the age to 10 and run the code to observe the output.
+# Task 1: Run the code and check the output. Observe the indentation on lines 14 & 16.
+# Task 2: Change the age to 10 on line 12 and run the code to observe the output.
 # Task 3: Uncomment lines 20 to 22. Run and see the output.
 # Task 4: Uncomment lines 23 & 25. Complete the code on lines 23 to 26 to check for one more condition.
 
