03e94583e8946aed4880831e06139110661a10a7
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 11:10:10 +0530

    Add files via upload

---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10  1.6 If-Elif-Else (Multi-Decision).py
@@ -0,0 +1,27 @@
+# If-Elif-Else checks multiple conditions one by one.
+# When a true condition is found, that block runs and the rest are skipped.
+
+# Task : Create a variable called temperature. Use if-elif-else to print:
+#       1. "Very Hot" if temperature is 35 or more.
+#       2. "Warm" if temperature is 25–34.
+#       3. "Cool" if temperature is 15–24.
+#       4. "Cold" if temperature is below 15.
+
+
+# If-Elif-Else (Multi-Decision)
+# Phone automatically changes notification mode based on location.
+
+location = "school"
+
+if location == "home":
+    print("Normal Sound Mode")        # You can receive all notifications
+elif location == "school":
+    print("Silent Mode Activated")    # No disturbance in class
+elif location == "meeting":
+    print("Vibration Mode")           # Alerts without sound
+else:
+    print("Manual Mode")              # Unknown place
+    
+
+
+
