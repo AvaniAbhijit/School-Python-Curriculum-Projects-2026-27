92e7b03459b68c30f93aeb5a08e080c13036126f
Avani <avani.abhijit@orchids.edu.in>
2026-02-20 11:32:19 +0530

    Add files via upload

---
+++ Grade 9 & Grade 10/Volume 1/G9 & G10 1.7.2 For Loop with reverse.py
@@ -0,0 +1,14 @@
+# A reverse for loop counts backward by using a negative step in range().
+
+# Task 1 : Write a for loop using range() to print on line 15 onwards:
+#       1. "Mission 3 Ending" to "Mission 1 Ending"
+#       After the loop, print:
+#       2. "All Missions Completed!"
+
+# Task 2 : Change the third parameter -1 to 1 and chekc the output.
+
+# Reverse for loop example
+for number in range(5, 0, -1):
+    print("Countdown", number)
+
+print("Start!")
