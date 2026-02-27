250caa2d55137c39df13eeec69892173b1049d74
Avani <avani.abhijit@orchids.edu.in>
2026-02-27 13:14:07 +0530

    Update G6 2.6 Dictionaries.py

--- Grade 6/Volume 1/G6 2.6 Dictionaries.py
+++ Grade 6/Volume 1/G6 2.6 Dictionaries.py
@@ -3,23 +3,25 @@
 # It is written using { }.
 # We can add, change, or remove items.
 
-# Task 1: Print the dictionary on line 19.
-# Task 2: Add an item to the dictionary using mobile["storage"] = "128GB" and print on line 22.
-# Task 3: Remove item from dictionary using pop(color) and print updated dictionary on line 26.
+# Task 1: Print the dictionary on line 18.
+# Task 2: Add an item to the dictionary for color as key and black value on line 22 and print the updated dictionary on line 23.
+# Task 3: Remove storage item from dictionary on line 27 and print updated dictionary on line 28.
 
 # Create a dictionary of a mobile phone
 mobile = {
     "brand": "Samsung",
     "model": "Galaxy A14",
     "price": 15000,
-    "color": "Black"
 }
 
 # Print the dictionary
 
 
 # Add a new item
+mobile["storage"] = "128GB"
 
 
 
 # Remove an item using pop().
+mobile.pop("color")
+
