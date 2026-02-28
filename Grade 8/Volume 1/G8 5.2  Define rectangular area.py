89d54ed4e743146f9ffd544823a4a4895ffa2ed8
Avani <avani.abhijit@orchids.edu.in>
2026-02-28 18:33:54 +0530

    Update G8 5.2  Define rectangular area.py

--- Grade 8/Volume 1/G8 5.2  Define rectangular area.py
+++ Grade 8/Volume 1/G8 5.2  Define rectangular area.py
@@ -1,9 +1,8 @@
 # pygame.Rect() used to create a rectangular area object with the tuple (x, y, width, height)
-#   1. can update position using rect.x or rect.y as on line 26
-#   2. makes the code cleaner as Rect object can be passed to pygame.draw.rect()
+# It makes the code cleaner as the Rect object can be passed to pygame.draw.rect()
 
-# Task 1 : Create another WHITE vertical rectangle object - rect2
-# Task 2 : Draw rect2 below rect making it look like a 'T'
+# Task 1: Create another WHITE vertical rectangle object - rect2.
+# Task 2: Draw rect2 below rect1, making it look like a 'T.'
 
 import pygame
 
@@ -21,9 +20,9 @@ WHITE=(255,255,255)
 rect_x = 200    # x coordinate where the rectangle starts
 rect_y = 100    # y coordinate where the rectangle starts
 
-#create a rectangular area object with the dimensions
-rect = pygame.Rect(rect_x, rect_y, 200, 50) # This will not draw but it will define area.
-rect.y = rect.y + 100   # Changes the y coordinate of the rect object.
+# Create a rectangular area object with the dimensions
+rect1 = pygame.Rect(rect_x, rect_y, 200, 50) # This will not draw, but it will define an area.
+
 
 
 running = True
@@ -34,12 +33,13 @@ while running:
 
     screen.fill(BG_COLOR)
 
-    #replace the tuple with the rectangular area object - rect
-    pygame.draw.rect(screen, WHITE, rect)
+    # Replace the tuple with the rectangular area object - rect1
+    pygame.draw.rect(screen, WHITE, rect1)
 
 
     # Update the display
     pygame.display.update()
 
 # Quit Pygame
+
 pygame.quit()
