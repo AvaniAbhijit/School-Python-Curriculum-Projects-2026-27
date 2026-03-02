069dfffe428bc4aafe223e7ccea54ef3e75a5226
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 11:24:27 +0530

    Update G4 3.3 Turtle pen colour .py

--- Grade 4/Volume 1/G4 3.3 Turtle pen colour .py
+++ Grade 4/Volume 1/G4 3.3 Turtle pen colour .py
@@ -1,8 +1,8 @@
-# pencolor() changes the drawing pen color on line 10
-# By default, the turtle pen color is black
+# pencolor() changes the drawing pen color on line 10.
+# By default, the turtle pen color is black.
 
 # Task 1: Change the pencolor to "green" in line 10
-# Task 2: put a #(hash) before the line 10 ,run again and notice that the default pencolor is "black".
+# Task 2: put a #(hash) before line 10, run again, and notice that the default pencolor is "black".
 
 import turtle
 t = turtle.Turtle()
