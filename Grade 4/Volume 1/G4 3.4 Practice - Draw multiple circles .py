4653c1c618e4ff0e4aa845d9bb8717ec6dc4eb66
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 11:30:37 +0530

    Update G4 3.4 Practice - Draw multiple circles .py

--- Grade 4/Volume 1/G4 3.4 Practice - Draw multiple circles .py
+++ Grade 4/Volume 1/G4 3.4 Practice - Draw multiple circles .py
@@ -1,11 +1,8 @@
-# Drawing 3 circles one after another with different radius from lines 7 to 9.
-
-# Task 1:  Change the second circle’s radius to 100 in line 8.
+# Task 1: Draw 2 more circles of different radius on lines 6 & 7.
 
 import turtle
 t = turtle.Turtle()
-t.circle(40)        # Drawing multiple circles continuously
-t.circle(60)
-t.circle(80)
-
+t.circle(40)        # Circle 1.
+                    # Circle 2.
+                    # Circle 3.
 turtle.done()
