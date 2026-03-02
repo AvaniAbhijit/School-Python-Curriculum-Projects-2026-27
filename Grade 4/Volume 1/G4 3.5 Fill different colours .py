3c2497e828b8af5e0a24860bc3eba71824b60e74
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 11:33:21 +0530

    Update G4 3.5 Fill different colours .py

--- Grade 4/Volume 1/G4 3.5 Fill different colours .py
+++ Grade 4/Volume 1/G4 3.5 Fill different colours .py
@@ -1,8 +1,8 @@
-# Drawing a filled green circle using begin_fill() & end_fill().
-# begin_fill() starts filling colour inside the shape
-# end_fill() completes the filling process
+# begin_fill() starts filling colour inside the shape on line 10.
+# end_fill() completes the filling process on line 12.
 
 # Task 1: Fill red colour in the smaller circle using begin_fill on line 14 and end_fill on line 16.
+
 import turtle
 t = turtle.Turtle()
 t.shape("turtle")
