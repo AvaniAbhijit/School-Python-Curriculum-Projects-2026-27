22b0235a24f5008d246c3f0fc43ae5fd985e1cda
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 11:36:06 +0530

    Update G4 3.7 Turtle movement .py

--- Grade 4/Volume 1/G4 3.7 Turtle movement .py
+++ Grade 4/Volume 1/G4 3.7 Turtle movement .py
@@ -1,15 +1,15 @@
-# Moving the turtle forward 200 steps using t.forward() on line 10.
+# t.forward() is used to move the turtle forward.
+# On line 10, t.forward(200) will move the turtle 200 pixels.
 
-# Task 1: Move the turtle forward by 50 steps in line 11 and
-#        create the circle of radius 100 in line 12
+# Task 1: Move the turtle forward by 50 steps on line 11.
 
 import turtle
 t = turtle
 t.pencolor("red")
 t.shape("turtle")
-t.forward(200)     # turtle will move forward 200 steps
-                   # move forward 50 steps
-                   # Create a circle
+t.forward(200)       # turtle will move forward 200 steps
+                     # move forward 50 steps
+t.circle(50)         # Create a circle
 
 turtle.done()
 
