e8bd03620426e7e1341093fb3a46cdcf71ca40dd
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 18:21:59 +0530

    Update G7 4.1 Game window setup.py

--- Grade 7/Volume 1/G7 4.1 Game window setup.py
+++ Grade 7/Volume 1/G7 4.1 Game window setup.py
@@ -1,7 +1,8 @@
-# import pygame loads the Pygame library so your Python program can make games with 
-# graphics, sound, and controls.
+# import pygame → Brings the Pygame library into your program so you can use its game features.
+# pygame.init() → Starts and prepares all Pygame modules so the game can run properly.
+# pygame.display.set_mode() → Creates the game window with the specified width and height.
 
-# Task 1: Define screen_width and screen_height variables on lines 14,15, respectively, 
+# Task 1: Define screen_width and screen_height variables on lines 15,16, respectively, 
 #         and assign the value 800,600, respectively.
 # Task 2: After you run the code, try to close the output window by clicking on the cross button. 
 
@@ -17,5 +18,5 @@ pygame.init()
 # Set up the game window
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Set the title of the game window to 'Bricks Game'
+# Set the title of the game window to 'Bricks Game.'
 pygame.display.set_caption('Bricks Game')
