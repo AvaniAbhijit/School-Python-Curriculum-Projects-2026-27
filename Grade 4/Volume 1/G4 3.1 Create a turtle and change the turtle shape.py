bc2ad9a3fa0bbe439764be7d9839cbbac5854b85
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 11:14:29 +0530

    Update G4 3.1 Create a turtle and change the turtle shape.py

--- Grade 4/Volume 1/G4 3.1 Create a turtle and change the turtle shape.py
+++ Grade 4/Volume 1/G4 3.1 Create a turtle and change the turtle shape.py
@@ -1,9 +1,9 @@
-# There are total 7 different shapes of turtle
-# turtle,square,triangle,circle,classic,arrow,blank
-# by default shape of turtle is classic
+# There are a total of 7 different shapes of turtle
+# turtle, square, triangle, circle, classic, arrow, blank
+# by default shape of the turtle is classic
 
 # Task 1: Change the shape of the turtle in line 10 to "square".
-# Task 2: put a #(hash) before the line 10 ,run again and notice that the default shape is "classic".
+# Task 2: Put a #(hash) before line 10, run again, and notice that the default shape is "classic".
 
 import turtle       # import turtle module to use turtle features
 t = turtle.Turtle() # Creating a turtle
