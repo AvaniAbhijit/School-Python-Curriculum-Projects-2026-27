a550c587541234e0d398b8a6cad8f9a357c64bfa
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 11:26:05 +0530

    Update G4 3.4 Practice - Draw multiple circles .py

--- Grade 4/Volume 1/G4 3.4 Practice - Draw multiple circles .py
+++ Grade 4/Volume 1/G4 3.4 Practice - Draw multiple circles .py
@@ -1,4 +1,4 @@
-# Drawing 3 circles one after another with different radius values.
+# Drawing 3 circles one after another with different radius from lines 7 to 9.
 
 # Task 1:  Change the second circle’s radius to 100 in line 8.
 
