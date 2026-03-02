3d17ea3fa36cb7a994100b7ae573bba66c4c97e9
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 11:23:21 +0530

    Update G4 3.2 Create a Circle with turtle .py

--- Grade 4/Volume 1/G4 3.2 Create a Circle with turtle .py
+++ Grade 4/Volume 1/G4 3.2 Create a Circle with turtle .py
@@ -1,10 +1,10 @@
-# Drawing a circle of radius 50 on line 9
-# circle(50) means radius of circle is 50 pixels
+# t.circle() is used to draw a circle on the screen on line 9.
+# We draw a circle with radius 50 on line 9.
 
-# Task 1: In line 9 change the radius of circle to 100.
+# Task 1: In line 9, change the radius of the circle to 100 and run the code.
 
 import turtle
 t = turtle.Turtle()
 t.shape("turtle")
-t.circle(50)             # Creating a circle with a radius of 50
-turtle.done()
+t.circle(50)             # Creating a circle with a radius of 50.
+turtle.done().
