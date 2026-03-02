fbf5dc3b4fc99a4f04c1c943670a9f642bbc0f5e
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 18:17:16 +0530

    Update G8 4.1 Game window setup.py

--- Grade 8/Volume 1/G8 4.1 Game window setup.py
+++ Grade 8/Volume 1/G8 4.1 Game window setup.py
@@ -1,4 +1,9 @@
-# Task 1: Define screen_width and screen_height variables on lines 11,12, respectively, 
+# import pygame → Brings the Pygame library into your program so you can use its game features.
+# pygame.init() → Starts and prepares all Pygame modules so the game can run properly.
+# pygame.display.set_mode() → Creates the game window with the specified width and height.
+
+
+# Task 1: Define screen_width and screen_height variables on lines 16,17, respectively, 
 #         and assign the value 600,500, respectively.
 # Task 2: After you run the code, try to close the output window by clicking on the cross button. 
 
@@ -14,6 +19,7 @@ pygame.init()
 # Set up the game window
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Set the title of the game window to 'Pong AI Game'
+# Set the title of the game window to 'Pong AI Game.'
 
 pygame.display.set_caption('Pong AI Game')
+
