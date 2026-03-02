3ac3fa31b52819670ca61850fc08e1a944ceaa46
Avani <avani.abhijit@orchids.edu.in>
2026-03-02 11:18:57 +0530

    Update G4 3.1 Create a turtle and change the turtle shape.py

--- Grade 4/Volume 1/G4 3.1 Create a turtle and change the turtle shape.py
+++ Grade 4/Volume 1/G4 3.1 Create a turtle and change the turtle shape.py
@@ -1,9 +1,11 @@
-# There are a total of 7 different shapes of turtle
-# turtle, square, triangle, circle, classic, arrow, blank
-# by default shape of the turtle is classic
+# Turtle is a Python module that is used to draw shapes and pictures.
+# import is a command that helps to use the turtle module in Python.
+# There are a total of 7 different shapes of turtle.
+#   1. turtle, square, triangle, circle, classic, arrow, blank
+#   2. By default shape of the turtle is classic
 
-# Task 1: Change the shape of the turtle in line 10 to "square".
-# Task 2: Put a #(hash) before line 10, run again, and notice that the default shape is "classic".
+# Task 1: Change the shape of the turtle in line 12 to "square".
+# Task 2: Put a #(hash) before line 12, run again, and notice that the default shape is "classic".
 
 import turtle       # import turtle module to use turtle features
 t = turtle.Turtle() # Creating a turtle
