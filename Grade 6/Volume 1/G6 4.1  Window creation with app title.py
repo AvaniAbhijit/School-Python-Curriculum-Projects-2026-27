5d6f590748475d56bd1f8e5e6e053d849089c443
Avani <avani.abhijit@orchids.edu.in>
2026-03-04 16:28:02 +0530

    Update G6 4.1  Window creation with app title.py

--- Grade 6/Volume 1/G6 4.1  Window creation with app title.py
+++ Grade 6/Volume 1/G6 4.1  Window creation with app title.py
@@ -2,8 +2,8 @@
 # ctk.CTk() → Is used to create the main window (root).
 
 # Task 1: Uncomment line no 16 and run the code.
-# Task 1: Change the window size to 600x500 on line 12.
-# Task 2: Uncomment and add the window title "Crypto Message App" on line 14.
+# Task 2: Change the window size to 600x500 on line 12.
+# Task 3: Uncomment and add the window title "Crypto Message App" on line 14.
 
 import customtkinter as ctk     # Import the CustomTkinter library for modern-looking Tkinter UI components
 
@@ -14,5 +14,6 @@ root.geometry("400x300")         # geometry() sets the window size.
 #root.title("________________________")  # giving title to the window.
 
 #root.mainloop()                         # Start the window loop (this keeps the window running)
+
 
 
