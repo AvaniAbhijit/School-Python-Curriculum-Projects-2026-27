1a145065ba3d3072c7f8cc8c572a91d94af4d984
Avani <avani.abhijit@orchids.edu.in>
2026-03-06 22:20:05 +0530

    Update G7 5.4 Draw bricks with col & rows.py

--- Grade 7/Volume 1/G7 5.4 Draw bricks with col & rows.py
+++ Grade 7/Volume 1/G7 5.4 Draw bricks with col & rows.py
@@ -1,8 +1,7 @@
-# In this code, We are drawing the bricks on the screen using columns and rows with nested for loop.
-# 
+# In this code, we are creating the bricks on the screen using columns and rows with nested for loop.
 
-# Task : Change the range in the for loop to fill half screen with bricks on line 28 and 29.
-
+# Task 1: To add a gap between bricks, add padding to the brick_width and brick_height.
+# Task 2: Change the range in the for loop to fill half screen with bricks on line 28 and 29.
 
 import pygame
 
@@ -14,8 +13,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Bricks Game')
 
 BG_COLOR = (255,255,255)
-BLUE = (0,0,255) 
-BLACK = (0,0,0) 
+BLUE = (0,0,255)
+BLACK = (0,0,0)
 
 # Bricks layout settings
 brick_width = 78
@@ -29,8 +28,8 @@ bricks_list=[]
 for row in range(0,3):  # Loop through each row
     for col in range(0,5):  # Loop through each column
         # Calculate brick's x and y position
-        brick_x = col * (brick_width + brick_padding) + brick_padding
-        brick_y = row * (brick_height + brick_padding) + brick_padding
+        brick_x = col * brick_width
+        brick_y = row * brick_height
 
         # Create a brick as a rectangle
         brick = pygame.Rect(brick_x, brick_y, brick_width, brick_height)
@@ -47,15 +46,10 @@ while running:
 
     screen.fill(BG_COLOR)
 
-    # Draws the white rectangle on the screen at the bottom of the screen.
-    pygame.draw.rect(screen, BLUE, (350,550,100,20))    
-    for brick in bricks_list: 
-        pygame.draw.rect(screen, BLACK, (brick))                                             
+    pygame.draw.rect(screen, BLUE, (350,550,100,20))
+    for brick in bricks_list:
+        pygame.draw.rect(screen, BLACK, (brick))
 
-
-    # Update the display
     pygame.display.update()
-
-# Quit Pygame
 
 pygame.quit()
