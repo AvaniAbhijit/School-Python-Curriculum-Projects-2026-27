c5f3399eef71e25d47577d4da133027c208ac376
Avani <avani.abhijit@orchids.edu.in>
2026-03-07 23:43:27 +0530

    Add files via upload

---
+++ Grade 6/Volume 1/G6 4.9 Add image to the left_middle frame and on the button bottom frame.py
@@ -0,0 +1,92 @@
+# Image.open() – Opens an image file (like .jpg or .png) so Python can read and work with it.
+# PhotoImage() – Converts the image into a format that Tkinter/CustomTkinter can 
+# display on the screen.
+
+# Task:
+# Add images to the three buttons inside the bottom_frame.
+# 1. Load three images using CTkImage (example: open_file.png, encrypt.png, decrypt.png).
+# 2. Set the size of each image to (25, 25).
+# 3. Add one image to each button.
+# 4. Keep the button text along with the image.
+# 5. Place the buttons inside buttons_container using pack() with side="left" and padx=25.
+
+
+import customtkinter as ctk
+from PIL import Image, ImageTk  
+
+frame_background = "dimgrey"  
+root = ctk.CTk()
+root.geometry("600x500")
+
+root.title("Crypto Message App")
+
+# Creating 3 frames for layout
+top_frame = ctk.CTkFrame(root, fg_color=frame_background, height=80)
+top_frame.pack(fill="x", pady=5)
+
+
+#Loading Images    
+logo = ctk.CTkImage(Image.open('logo.png'), size=(120, 120))  # Open the image and set its size
+orchids_image = ctk.CTkImage(Image.open('orchids.png'), size=(100, 100))
+
+# Create a first label with the logo image, no text, and pack it on the window.
+logo_image = ctk.CTkLabel(top_frame, image=logo, text="", fg_color=frame_background)
+logo_image.pack(side="left", padx=(10, 0))
+
+# Create a second label with the logo image, no text, and pack it on the window.C
+logo_orchids_image = ctk.CTkLabel(top_frame, image=orchids_image, text="", fg_color=frame_background)
+logo_orchids_image.pack(side="right", padx=(10, 0))
+
+
+# Label inside top frame
+message_label = ctk.CTkLabel(top_frame,text="Message Encrypter", font=("Cascadia Code SemiBold", 20, "bold"), text_color="white")
+message_label.pack(side="left", padx=(70,0))
+
+middle_frame = ctk.CTkFrame(root, fg_color=frame_background)
+middle_frame.pack(fill="x", expand=True)
+
+bottom_frame = ctk.CTkFrame(root, fg_color=frame_background)
+bottom_frame.pack(fill="x")
+
+# Middle frame content - split into left and right
+# Below is the left_middle frame
+left_middle = ctk.CTkFrame(middle_frame, fg_color="Black", width=260)
+left_middle.pack(side="left", padx=(25, 5), fill="both")
+
+#Code for right_middle frame
+right_middle = ctk.CTkFrame(middle_frame, fg_color="White", width=260)
+right_middle.pack(side="right", padx=(5, 25), fill="both")
+
+# Label inside top frame
+message_label1 = ctk.CTkLabel(right_middle,text="Enter Secret Message", font=("Cascadia Code SemiBold", 20, "bold"), text_color="Black")
+message_label1.pack(fill="x", pady=5)
+
+# Open the image file named 'cryptopic.jpg'
+sender = Image.open('cryptopic.jpg') 
+
+# Resize the image to 240x240 pixels
+sender = sender.resize((240, 240))
+
+# Convert the image into a format that Tkinter/CustomTkinter can display
+label_image = ImageTk.PhotoImage(sender)
+
+# Create a photolabel with the cryptopic image, no text, and pack it on the window.
+PhotoLabel = ctk.CTkLabel(left_middle, image=label_image, text="", width=247, height=247, fg_color="white")
+PhotoLabel.pack(pady=10)
+
+buttons_container = ctk.CTkFrame(bottom_frame, fg_color=frame_background)
+buttons_container.pack(fill="x", expand=True, pady=15)
+
+# Bottom frame button-1 - open_button
+open_button  = ctk.CTkButton(buttons_container, text="Button1", fg_color=frame_background)
+open_button.pack(side="left",padx = 25)  
+
+# Bottom frame button-2 encrypt_button
+encrypt_button = ctk.CTkButton(buttons_container, text="Button2", fg_color=frame_background)
+encrypt_button.pack(side="left",padx = 25)  
+
+# Bottom frame button-3 decrypt_button
+decrypt_button = ctk.CTkButton(buttons_container, text="Button3", fg_color=frame_background)
+decrypt_button.pack(side="left",padx = 25)  
+
+root.mainloop()
