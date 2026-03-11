a5c42e3d4e7fa6a7d6c4d4047d15b1ed5c6b8488
Avani <avani.abhijit@orchids.edu.in>
2026-03-11 12:30:26 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 5.2 Functions for event handling.py
@@ -0,0 +1,61 @@
+# xcor() returns the current x-coordinate (horizontal position) of the turtle on the screen.
+# setx() changes the turtle's x-coordinate, moving it left or right without changing its y-position.
+# move_left() function on line 43 uses xcor() and setx() to change the left position
+#             of the basket when the LEFT arrow key is pressed.
+
+# Task 1: Define the function move_right() on line 47 and write the function body to
+#         Calculate the x-coordinate using xcor() and add 50 to it
+#         Set the new position of the basket using setx()
+# Task 2: Change the Left and Right controls on lines 53 and 54 to 'a' and 'd' keys. Run and observe.
+
+
+import turtle
+
+# Set up the screen
+screen = turtle.Screen()
+screen.title("Fruit Catcher Game")
+screen.bgcolor("lightblue")
+screen.setup(width=800, height=600)
+
+
+# Create the basket
+basket = turtle.Turtle()
+basket.shape("square")
+basket.color("brown")
+basket.shapesize(stretch_wid=1, stretch_len=5)
+basket.penup()
+basket.goto(0, -250)
+
+# Create the fruits
+fruit1 = turtle.Turtle()
+fruit1.shape("circle")
+fruit1.color("yellow")
+fruit1.penup()
+fruit1.goto(-200, 250)
+
+fruit2 = turtle.Turtle()
+fruit2.shape("circle")
+fruit2.color("green")
+fruit2.penup()
+fruit2.goto(200, 250)
+
+# Basket Movement
+def move_left():             # function to move left
+    x = basket.xcor() - 50   # Get the current x position of basket and subtract 50 to move left
+    basket.setx(x)           # Move the basket to the new x position
+
+                             # function to move right
+                             # Get the current x position of basket and add 50 to move right
+                             # Move the basket to the new x position
+
+
+screen.listen()                     # Listen for keyboard input
+screen.onkey(move_left, "Left")     # call move_left() when Left arrow is pressed
+screen.onkey(move_right, "Right")   # call move_right() when Right arrow is pressed
+
+
+
+screen.update()
+
+
+
---
+++ Grade 5/Volume 1/G5 5.3 Check boundary limits.py
@@ -0,0 +1,56 @@
+# Use conditional statement to check the basket stays within the screen boundary.
+# On line 39: x > -350: checks if the x-coordinate of basket is
+# always > the left boundary of the screen
+
+# Task 1: Add the condition on line 44 to stop the basket move beyond boundary
+
+import turtle
+
+# Set up the screen
+screen = turtle.Screen()
+screen.title("Fruit Catcher Game")
+screen.bgcolor("lightblue")
+screen.setup(width=800, height=600)
+
+# Create the basket
+basket = turtle.Turtle()
+basket.shape("square")
+basket.color("brown")
+basket.shapesize(stretch_wid=1, stretch_len=5)
+basket.penup()
+basket.goto(0, -250)
+
+# Create the fruits
+fruit1 = turtle.Turtle()
+fruit1.shape("circle")
+fruit1.color("yellow")
+fruit1.penup()
+fruit1.goto(-200, 250)
+
+fruit2 = turtle.Turtle()
+fruit2.shape("circle")
+fruit2.color("green")
+fruit2.penup()
+fruit2.goto(200, 250)
+
+# Basket Movement
+def move_left():
+    x = basket.xcor() - 50
+    if x > -350:                    # Check if the basket has not gone beyond 350 on left
+        basket.setx(x)              # Then only set the new x coordinate position
+
+def move_right():
+    x = basket.xcor() + 50
+    basket.setx(x)                  # Check if the basket has not gone beyond 350 on right
+
+screen.listen()
+screen.onkey(move_left, "Left")
+screen.onkey(move_right, "Right")
+
+
+
+screen.update()
+
+
+
+
