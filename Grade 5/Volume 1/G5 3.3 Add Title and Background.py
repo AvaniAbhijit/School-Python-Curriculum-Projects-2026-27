b2144996a3f11952df361a86aff5acf01b53d123
Avani <avani.abhijit@orchids.edu.in>
2026-03-11 11:04:21 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 3.3 Add Title and Background.py
@@ -0,0 +1,13 @@
+# title() – It sets the name of the game window that appears at the top of the screen.
+# bgcolor() – It sets the background color of the game screen
+
+# Task 1 : Change the title to "Fruit Catcher Game" on line 11.
+# Task 2 : Change the bgcolor of your choice on line 13.
+
+import turtle
+screen = turtle.Screen()
+screen.setup(width=800, height=600)
+
+screen.title("Game Title")   # Set the title shown at the top of the window
+
+screen.bgcolor("lightblue")   # Set the background color of the screen
