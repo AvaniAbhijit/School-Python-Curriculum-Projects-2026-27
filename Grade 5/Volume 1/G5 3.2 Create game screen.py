1886da25342074f3ab3dbea1987e45333de9f21d
Avani <avani.abhijit@orchids.edu.in>
2026-03-11 10:59:47 +0530

    Add files via upload

---
+++ Grade 5/Volume 1/G5 3.2 Create game screen.py
@@ -0,0 +1,11 @@
+# import turtle – It tells Python to use the turtle drawing tools so we can make graphics an
+# setup() – It is used to set the size of the window where the turtle program will run.
+
+# Task : Change the values of width & height on line 10.
+
+import turtle   # Import the turtle library so we can use turtle graphics
+
+screen = turtle.Screen()  # Create the game screen.
+
+screen.setup(width=800, height=600)  # Set the window size to 800 pixels wide and 600 pixels tall.
+
