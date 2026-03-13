1c701d85a33eb5f0a9f2f61dd72084d3c4c891c8
Avani <avani.abhijit@orchids.edu.in>
2026-03-13 10:25:07 +0530

    Update G5 1.4.1 Revisitng Variables.py

--- Grade 5/Volume 1/G5 1.4.1 Revisitng Variables.py
+++ Grade 5/Volume 1/G5 1.4.1 Revisitng Variables.py
@@ -1,5 +1,6 @@
-# Task 1: Create variables to store:Your name,Your class,Your age
+# Task 1: Create variables to store:Your name,Your grade,Your age
 #         Display all the information clearly on the screen using print().
 
 # Task 2: Store your marks for:Math,Science
+
 #         Add both marks and display the total marks using print().
