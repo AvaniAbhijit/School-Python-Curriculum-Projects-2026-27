cd08f7856ce6e54e529dd255e61b304c640095f7
Avani <avani.abhijit@orchids.edu.in>
2026-03-20 18:44:31 +0530

    Update G5 4.1 Create a basket.py

--- Grade 5/Volume 1/G5 4.1 Create a basket.py
+++ Grade 5/Volume 1/G5 4.1 Create a basket.py
@@ -1,12 +1,15 @@
 # shape() – It is used to choose the shape of the turtle object.
 # Example: "square" makes the turtle look like a square.
 
-# shapesize() – It is used to change the size of the shape.
-# Example: stretch_len=5 makes the shape longer.
+# shapesize() – It is used to change the size of the shape. Takes 2 parameters:
+# stretch_wid = 1 → keep the height normal
+# stretch_len = 5 → make the shape 5 times longer horizontally (width)
+# The default square shape turtle is 20 pixels(width) × 20 pixels(height).
+# stretch_len = 5 → width = 5 × 20 = 100 pixels
 
-# Task 1: Try all the turtle shapes(circle,classic,turtle,arrow) on line 18.
-# Task 2: Change the basket color for the shape on line 19.
-# Task 3: Uncomment line 20 and run the code.
+# Task 1: Try all the turtle shapes(circle,classic,turtle,arrow) on line 21.
+# Task 2: Uncomment line 23 and run the code.
+# Task 3: Change the basket's stretch_width to 3 on line 23. Run and observe the change.
 
 import turtle
 screen = turtle.Screen()
@@ -18,5 +21,3 @@ basket = turtle.Turtle()   # Create a turtle object called basket
 basket.shape("square")   # Set the basket shape to square
 basket.color("brown")   # Change the color of the basket
 #basket.shapesize(stretch_wid=1, stretch_len=5)   # Stretch the square to make it look like a basket
-
-
