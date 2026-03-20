3baf2951dd674b85d570ca369d25ae876964aac9
Avani <avani.abhijit@orchids.edu.in>
2026-03-20 18:20:51 +0530

    Update G6 4.9 Add image to the left_middle frame and on the button bottom frame.py

--- Grade 6/Volume 1/G6 4.9 Add image to the left_middle frame and on the button bottom frame.py
+++ Grade 6/Volume 1/G6 4.9 Add image to the left_middle frame and on the button bottom frame.py
@@ -1,20 +1,23 @@
-# Image.open() – Opens an image file (like .jpg or .png) so Python can read and work with it.
-# PhotoImage() – Converts the image into a format that Tkinter/CustomTkinter can 
-# display on the screen.
+# PhotoImage() on line 73 – Converts the image into a format that Tkinter/CustomTkinter can display on the screen.
+# This is used as this image will be replaced with any other image of the user's choice.
+
+# On line 75, used CTKImage() to create an image for the open_button with size (70,70).
+# On line 85 this image can be made to appear on a button using the image attribute.
+# Note: The text attribute has been made empty.
 
 # Task:
-# Add images to the three buttons inside the bottom_frame.
-# 1. Load three images using CTkImage (example: open_file.png, encrypt.png, decrypt.png).
-# 2. Set the size of each image to (25, 25).
-# 3. Add one image to each button.
-# 4. Keep the button text along with the image.
-# 5. Place the buttons inside buttons_container using pack() with side="left" and padx=25.
+# Add images to other 2 buttons inside the bottom_frame.
+# 1. Load two images using CTkImage (example: encrypt.png, decrypt.png) like open_file.
+# 2. Set the size of each image to (70, 70).
+# 3. Add one image to each button - encrypt_button and decrypt_button like done for open_button.
+# 4. Make the button text empty along with the image.
+
 
 
 import customtkinter as ctk
-from PIL import Image, ImageTk  
+from PIL import Image, ImageTk
 
-frame_background = "dimgrey"  
+frame_background = "dimgrey"
 root = ctk.CTk()
 root.geometry("600x500")
 
@@ -25,7 +28,7 @@ top_frame = ctk.CTkFrame(root, fg_color=frame_background, height=80)
 top_frame.pack(fill="x", pady=5)
 
 
-#Loading Images    
+#Loading Images
 logo = ctk.CTkImage(Image.open('logo.png'), size=(120, 120))  # Open the image and set its size
 orchids_image = ctk.CTkImage(Image.open('orchids.png'), size=(100, 100))
 
@@ -36,7 +39,6 @@ logo_image.pack(side="left", padx=(10, 0))
 # Create a second label with the logo image, no text, and pack it on the window.C
 logo_orchids_image = ctk.CTkLabel(top_frame, image=orchids_image, text="", fg_color=frame_background)
 logo_orchids_image.pack(side="right", padx=(10, 0))
-
 
 # Label inside top frame
 message_label = ctk.CTkLabel(top_frame,text="Message Encrypter", font=("Cascadia Code SemiBold", 20, "bold"), text_color="white")
@@ -62,13 +64,15 @@ message_label1 = ctk.CTkLabel(right_middle,text="Enter Secret Message", font=("C
 message_label1.pack(fill="x", pady=5)
 
 # Open the image file named 'cryptopic.jpg'
-sender = Image.open('cryptopic.jpg') 
+sender = Image.open('cryptopic.jpg')
 
 # Resize the image to 240x240 pixels
 sender = sender.resize((240, 240))
 
 # Convert the image into a format that Tkinter/CustomTkinter can display
 label_image = ImageTk.PhotoImage(sender)
+
+open_file = ctk.CTkImage(Image.open('open_file.png'), size=(70, 70))
 
 # Create a photolabel with the cryptopic image, no text, and pack it on the window.
 PhotoLabel = ctk.CTkLabel(left_middle, image=label_image, text="", width=247, height=247, fg_color="white")
@@ -78,15 +82,15 @@ buttons_container = ctk.CTkFrame(bottom_frame, fg_color=frame_background)
 buttons_container.pack(fill="x", expand=True, pady=15)
 
 # Bottom frame button-1 - open_button
-open_button  = ctk.CTkButton(buttons_container, text="Button1", fg_color=frame_background)
-open_button.pack(side="left",padx = 25)  
+open_button  = ctk.CTkButton(buttons_container, image=open_file, text="", fg_color=frame_background)
+open_button.pack(side="left",padx = 25)
 
 # Bottom frame button-2 encrypt_button
 encrypt_button = ctk.CTkButton(buttons_container, text="Button2", fg_color=frame_background)
-encrypt_button.pack(side="left",padx = 25)  
+encrypt_button.pack(side="left",padx = 25)
 
 # Bottom frame button-3 decrypt_button
 decrypt_button = ctk.CTkButton(buttons_container, text="Button3", fg_color=frame_background)
-decrypt_button.pack(side="left",padx = 25)  
+decrypt_button.pack(side="left",padx = 25)
 
 root.mainloop()
